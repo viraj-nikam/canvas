799bde19a3f93dbf3359157f6770c4cfd1ea3561
Todd Austin <austin.todd.j@gmail.com>
2016-03-03 19:59:28 -0600

    Adds page header to blog

--- resources/views/blog/layouts/master.blade.php
+++ resources/views/blog/layouts/master.blade.php
@@ -3,19 +3,32 @@
 <head>
     <meta charset="utf-8">
     <meta property="og:type" content="blog">
+
     @yield('og-description')
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="{{ $meta_description }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
     @yield('title')
+
     <link rel="alternate" type="application/rss+xml" href="{{ url('rss') }}" title="RSS Feed {{ config('blog.title') }}">
     <link href="/assets/css/blog.css" rel="stylesheet">
+
     @yield('styles')
+
 </head>
 <body>
+
+@include('blog.partials.page-nav')
+
 @yield('content')
+
 @include('blog.partials.page-footer')
+
 <script src="/assets/js/blog.js"></script>
+
 @yield('scripts')
+
 </body>
 </html>
---
+++ resources/views/blog/partials/page-nav.blade.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+    <a href="/blog">CANVAS</a>
+    <br>
+    <div class="links">
+        <a href="https://twitter.com/austintoddj" target="_blank"><i class="fa fa-fw fa-twitter"></i></a>
+        <a href="https://github.com/austintoddj" target="_blank"><i class="fa fa-fw fa-github"></i></a>
+        <a href="/rss" target="_blank"><i class="fa fa-fw fa-rss"></i></a>
+    </div>
+</div>
