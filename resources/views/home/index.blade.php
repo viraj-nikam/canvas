4b137526b82bdb92e65fa38d9ceaf6e1b6bbb5c0
Todd Austin <austin.todd.j@gmail.com>
2016-03-06 17:58:34 -0600

    Finishes home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,26 @@
 @extends('home.master')
 
 @section('content')
-    <a href="/" class="nav-link">Home</a>
-    <a href="/blog" class="nav-link">Blog</a>
-    <a href="#" class="nav-link">GitHub</a>
-
     <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-1">
+                <h1 class="title">Canvas</h1>
+                <p class="lead"><em>A Minimal Blogging Application For Developers.</em></p>
+                <p>Canvas is a minimalistic blogging application for developers. Canvas attempts to make blogging simple and enjoyable by utilizing the latest technologies and keeping the administration as simple as possible with the primary focus on writing.</p>
+                <br />
+                <center>
+                    <a href="blog" class="btn btn-default btn-outline btn-sm"><i class="fa fa-fw fa-edit"></i> Blog</a>
+                    <a href="admin" class="btn btn-default btn-outline btn-sm"><i class="fa fa-fw fa-user"></i> Admin</a>
+                    <a href="http://github.com/austintoddj/canvas" target="_blank" class="btn btn-outline btn-default btn-sm"><i class="fa fa-fw fa-github"></i> Download from GitHub</a>
+                </center>
+            </div>
+            <div class="col-md-5">
+                <img src="{{ asset('images/laptop.gif') }}" width="650" class="laptop img-responsive">
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid feature-block">
         <div class="col-md-10 col-md-offset-1">
             <div class="row">
                 <div class="col-md-4">
@@ -31,7 +46,7 @@
                     <p>Canvas utilizes custom LESS files so you can modify the theme to your own taste and preference.</p>
                 </div>
                 <div class="col-md-4">
-                    <i class="fa fa-fw fa-fighter-jet fa-2x features"></i> <span class="lead features">Simple Configuration</span>
+                    <i class="fa fa-fw fa-cogs fa-2x features"></i> <span class="lead features">Simple Configuration</span>
                     <p>A single configuration file holds all of the necessary global variables to set in order to get you up and running in no time.</p>
                 </div>
             </div>
@@ -42,7 +57,7 @@
         <div class="container-fluid">
             <div class="col-md-10 col-md-offset-1">
                 <hr>
-                <p class="text-muted" align="center">&copy; {{ \Carbon\Carbon::today()->format('Y') }} {{ config('blog.title') }} - By <a href="http://toddaustin.noip.me" target="_blank">Todd Austin</a>. All Rights Reserved</p>
+                <p class="text-muted" align="center">&copy; {{ \Carbon\Carbon::today()->format('Y') }} {{ config('blog.title') }}. All Rights Reserved</p>
             </div>
         </div>
     </footer>
