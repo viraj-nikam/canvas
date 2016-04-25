0a7ef0a1c9110f9fca5a173a48daa5c4894e45f8
Todd Austin <austin.todd.j@gmail.com>
2016-04-25 13:15:30 -0500

    Reformats blog config file

--- config/blog.php
+++ config/blog.php
@@ -1,16 +1,16 @@
 <?php
 return [
-  'name' => 'Canvas',
-  'title' => 'Canvas',
-  'subtitle' => 'Minimal blog application for developers',
-  'description' => 'Blog',
-  'author' => 'Todd Austin',
-  'posts_per_page' => 6,
-  'rss_size' => 25,
-  'twitter' => 'https://twitter.com/example',
-  'github' => 'https://github.com/example',
-  'uploads' => [
-    'storage' => 'local',
-    'webpath' => '/uploads/',
+  'name'            => 'Canvas',
+  'title'           => 'Canvas',
+  'subtitle'        => 'Minimal blog application for developers',
+  'description'     => 'Blog',
+  'author'          => 'Todd Austin',
+  'posts_per_page'  => 6,
+  'rss_size'        => 25,
+  'twitter'         => 'https://twitter.com/example',
+  'github'          => 'https://github.com/example',
+  'uploads'         => [
+    'storage'       => 'local',
+    'webpath'       => '/uploads/',
   ],
 ];
