671b56ee4b74e136ed42365184717264ceabc7f5
Todd Austin <austin.todd.j@gmail.com>
2016-04-26 13:08:21 -0500

    Adds documentation for configuration

--- config/blog.php
+++ config/blog.php
@@ -1,16 +1,56 @@
 <?php
+
 return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Blog Meta Configuration
+  |--------------------------------------------------------------------------
+  |
+  | Here you may define all of the blog meta tags for your application.
+  | The Twitter and GitHub accounts are linked to the header that
+  | appears on each page.
+  |
+  */
+
   'name'            => 'Canvas',
   'title'           => 'Canvas',
   'subtitle'        => 'Minimal blog application for developers',
   'description'     => 'Blog',
   'author'          => 'Todd Austin',
-  'posts_per_page'  => 6,
-  'rss_size'        => 25,
   'twitter'         => 'https://twitter.com/example',
   'github'          => 'https://github.com/example',
+
+  /*
+  |--------------------------------------------------------------------------
+  | Blog Post Configuration
+  |--------------------------------------------------------------------------
+  |
+  | Pretty self-explanatory here. Indicate how many posts you would
+  | like to appear on each page and how many posts to display
+  | via the rss feed.
+  |
+  */
+
+  'posts_per_page'  => 6,
+  'rss_size'        => 25,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Uploads Configuration
+  |--------------------------------------------------------------------------
+  |
+  | Specify what type of storage you would like for your application. Just
+  | as a reminder, your uploads directory MUST be writable by the
+  | web server for the uploading to function properly.
+  |
+  | Supported: "local"
+  |
+  */
+
   'uploads'         => [
     'storage'       => 'local',
     'webpath'       => '/uploads/',
   ],
+
 ];
