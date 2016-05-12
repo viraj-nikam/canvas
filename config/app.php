6bd6e4693654e6456b5770576c42ee2395be9193
Todd Austin <austin.todd.j@gmail.com>
2016-05-12 09:04:03 -0500

    Updates comments in routes and config

--- config/app.php
+++ config/app.php
@@ -145,6 +145,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Canvas Service Providers...
+         */
         Bestmomo\Scafold\ScafoldServiceProvider::class,
 
     ],
