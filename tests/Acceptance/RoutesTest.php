05b439c8a9379c0e7c8549f171a5a5167f660e7a
Todd Austin <austin.todd.j@gmail.com>
2016-05-19 08:06:33 -0500

    Adds acceptance testing

--- tests/Acceptance/RoutesTest.php
+++ tests/Acceptance/RoutesTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class CanvasRoutesTest extends TestCase
+class RoutesTest extends TestCase
 {
     /**
      * Test the response code for the Landing page.
