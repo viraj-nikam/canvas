c7828a049f91dd06e850c29a37a2eb03cb8ca6c1
Todd Austin <austin.todd.j@gmail.com>
2016-05-19 14:24:55 -0500

    Updates user model with new fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,13 +18,13 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var string
      */
     protected $table = 'users';
-    
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['email', 'password', 'first_name', 'last_name', 'display_name', 'url', 'bio'];
 
     /**
      * The attributes excluded from the model's JSON form.
