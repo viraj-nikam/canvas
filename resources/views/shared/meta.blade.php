b0e3f3631c24b3bf96b6d85e8fac5a028951dc86
Todd Austin <austin.todd.j@gmail.com>
2016-05-25 08:48:10 -0500

    Deletes old files

--- resources/views/shared/meta.blade.php
+++
@@ -1,9 +0,0 @@
-@yield('og-description')
-
-<meta charset="utf-8">
-<meta property="og:type" content="blog">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="author" content="{{ config('blog.author') }}">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-<link rel="icon" href="{{ asset('images/logo.png') }}">
