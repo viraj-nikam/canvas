96f717d7e58748dcd0c06fcd19ca93d2d82a021b
Todd Austin <austin.todd.j@gmail.com>
2016-05-27 11:18:27 -0500

    Updates error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Canvas | Be right back</title>
+        <title>Canvas | Page not found</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Page not found.</div>
             </div>
         </div>
     </body>
