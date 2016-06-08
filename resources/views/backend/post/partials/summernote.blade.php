a9cc0561a6b08ec87fd540f09371a076755d2225
Todd Austin <austin.todd.j@gmail.com>
2016-06-08 08:55:02 -0500

    Hides image upload in summernote editor

--- resources/views/backend/post/partials/summernote.blade.php
+++ resources/views/backend/post/partials/summernote.blade.php
@@ -1,9 +1,15 @@
 <script type="text/javascript">
     $(document).ready(function(){
-      $('.publish_date').mask('00/00/0000 00:00:00');
+
+        $('button[data-name="picture"]').hide();
+        $('.publish_date').mask('00/00/0000 00:00:00');
+
     });
+
     $('#editor').summernote({
+
         placeholder: 'Content',
         height: 300,
+
     });
 </script>
