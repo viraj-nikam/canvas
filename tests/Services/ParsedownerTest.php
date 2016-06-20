f99f05a7c26ffd7389763e0e50b4c5cd6fbce24d
Todd Austin <austin.todd.j@gmail.com>
2016-06-20 11:24:36 -0500

    Adds more markdown tests

--- tests/Services/ParsedownerTest.php
+++ tests/Services/ParsedownerTest.php
@@ -21,14 +21,26 @@ class ParsedownerTest extends PHPUnit_Framework_TestCase
     {
         return [
             ["text", "<p>text</p>"],
-            ["# title", "<h1>title</h1>"],
-            ["## title", "<h2>title</h2>"],
-            ["### title", "<h3>title</h3>"],
-            ["#### title", "<h4>title</h4>"],
+            ["Header 1\n=======", "<h1>Header 1</h1>"],
+            ["# Header 1", "<h1>Header 1</h1>"],
+            ["## Header 2", "<h2>Header 2</h2>"],
+            ["### Header 3", "<h3>Header 3</h3>"],
+            ["#### Header 4", "<h4>Header 4</h4>"],
+            ["##### Header 5", "<h5>Header 5</h5>"],
+            ["###### Header 6", "<h6>Header 6</h6>"],
             ["`hello_world`", "<p><code>hello_world</code></p>"],
+            ["``` <?php \$var = \"Php Code Blocks\"; echo \$var; ?> ```", "<p><code>&lt;?php \$var = \"Php Code Blocks\"; echo \$var; ?&gt;</code></p>"],
             ["*italics text*", "<p><em>italics text</em></p>"],
             ["**bold text**", "<p><strong>bold text</strong></p>"],
             ["---", "<hr />"],
+            ["***", "<hr />"],
+            [">note", "<blockquote>\n<p>note</p>\n</blockquote>"],
+            ["[Canvas](http://austintoddj.github.io/Canvas \"Canvas\")", "<p><a href=\"http://austintoddj.github.io/Canvas\" title=\"Canvas\">Canvas</a></p>"],
+            ["Intra-word *emp*hasis", "<p>Intra-word <em>emp</em>hasis</p>"],
+            ["~~Strikethrough~~", "<p><del>Strikethrough</del></p>"],
+            ["![Canvas Logo](http://austintoddj.github.io/Canvas/img/canvas-logo.gif)", "<p><img src=\"http://austintoddj.github.io/Canvas/img/canvas-logo.gif\" alt=\"Canvas Logo\" /></p>"],
+            ["- List Item", "<ul>\n<li>List Item</li>\n</ul>"],
+            ["1. List Item", "<ol>\n<li>List Item</li>\n</ol>"],
         ];
     }
 }
