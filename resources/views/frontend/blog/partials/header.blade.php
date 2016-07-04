a739cbb37e41d92573ef973d2d4b6261010dbc3a
Christopher Mühl <christopher@padarom.io>
2016-07-04 08:49:15 +0200

    Hide service usernames from Frontend if not set

--- resources/views/frontend/blog/partials/header.blade.php
+++ resources/views/frontend/blog/partials/header.blade.php
@@ -3,13 +3,13 @@
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             <h1><a href="/">{{ config('blog.title') }}</a></h1>
 
-            @if(isset($user->twitter))
+            @if(isset($user->twitter) && strlen($user->twitter))
                 <a href="http://twitter.com/{{ $user->twitter }}" target="_blank" id="social"><i class="fa fa-fw fa-twitter"></i></a>
             @endif
-            @if(isset($user->facebook))
+            @if(isset($user->facebook) && strlen($user->facebook))
                 <a href="http://facebook.com/{{ $user->facebook }}" target="_blank" id="social"><i class="fa fa-fw fa-facebook"></i></a>
             @endif
-            @if(isset($user->github))
+            @if(isset($user->github) && strlen($user->github))
                 <a href="http://github.com/{{ $user->github }}" target="_blank" id="social"><i class="fa fa-fw fa-github"></i></a>
             @endif
         </div>
