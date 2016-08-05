46026865a6fdf686b9a5ac0f0093210efeef83fa
Ferreyra, Maximiliano Gastón <fergthh@gmail.com>
2016-08-05 19:34:20 -0300

    Fixed fullscreen & side-by-side

--- resources/views/backend/post/partials/editor.blade.php
+++ resources/views/backend/post/partials/editor.blade.php
@@ -1,3 +1,20 @@
+<style type="text/css">
+    .editor-toolbar.fullscreen{
+        top: 70px;
+        z-index: 12;
+    }
+
+    .editor-preview-side{
+        top: 120px;
+        z-index: 12;
+    }
+
+    .CodeMirror-fullscreen{
+        top: 120px;   
+        z-index: 11;
+    }
+</style>
+
 <script type="text/javascript">
     $(document).ready(function(){
         var simplemde = new SimpleMDE({ element: document.getElementById("editor") });
