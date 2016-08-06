560c813c9311040fd106773a03a4c18e3cc72e20
Valentin Prugnaud <valentin@whatdafox.com>
2016-08-06 01:01:27 -0700

    Improve assets compilation #67

--- gulpfile.js
+++ gulpfile.js
@@ -11,47 +11,58 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
 
-       mix.less('blog/blog.less');
-       mix.less('auth/auth.less');
-       mix.less('admin/admin.less');
+    // LESS Files
+    mix.less('blog/blog.less');
+    mix.less('auth/auth.less');
+    mix.less('admin/admin.less');
 
-       // Vendor Files
-       mix.copy('resources/assets/vendor/', 'public/vendor')
+    // Vendor CSS Files
+    mix.styles([
+        'bootstrap-datetimepicker.css',
+        'chosen.min.css',
+        'simplemde.min.css',
+        'jquery.bootgrid.min.css',
+        'lightgallery.css',
+        'bootstrap-select.min.css',
+        'jquery.mCustomScrollbar.min.css',
+        'material-design-iconic-font.min.css',
+        'animate.min.css',
+        'sweet-alert.min.css',
+    ], 'public/css/vendor.css');
 
-       // Raw CSS Files
-       mix.copy('resources/assets/css/bootstrap-datetimepicker.css', 'public/css/bootstrap-datetimepicker.min.css');
-       mix.copy('resources/assets/css/chosen.min.css', 'public/css/chosen.min.css');
-       mix.copy('resources/assets/css/simplemde.min.css', 'public/css/simplemde.min.css');
-       mix.copy('resources/assets/css/jquery.bootgrid.min.css', 'public/css/jquery.bootgrid.min.css');
-       mix.copy('resources/assets/css/lightgallery.css', 'public/css/lightgallery.css');
-       mix.copy('resources/assets/css/bootstrap-select.min.css', 'public/css/bootstrap-select.min.css');
-       mix.copy('resources/assets/css/jquery.mCustomScrollbar.min.css', 'public/css/jquery.mCustomScrollbar.min.css');
-       mix.copy('resources/assets/css/material-design-iconic-font.min.css', 'public/css/material-design-iconic-font.min.css');
-       mix.copy('resources/assets/css/animate.min.css', 'public/css/animate.min.css');
-       mix.copy('resources/assets/css/sweet-alert.min.css', 'public/css/sweet-alert.min.css');
-       mix.copy('resources/assets/css/custom.css', 'public/css/custom.css');
-       mix.copy('resources/assets/css/app-1.css', 'public/css/app-1.css');
-       mix.copy('resources/assets/css/app-2.css', 'public/css/app-2.css');
+    // Application CSS Files
+    mix.styles([
+        'custom.css',
+        'app-1.css',
+        'app-2.css',
+    ], 'public/css/app.css');
 
-       // Raw JS Files
-       mix.copy('resources/assets/js/bootstrap-select.js', 'public/js/bootstrap-select.js');
-       mix.copy('resources/assets/js/jquery.mask.min.js', 'public/js/jquery.mask.min.js');
-       mix.copy('resources/assets/js/chosen.jquery.min.js', 'public/js/chosen.jquery.min.js');
-       mix.copy('resources/assets/js/moment.min.js', 'public/js/moment.min.js');
-       mix.copy('resources/assets/js/simplemde.min.js', 'public/js/simplemde.min.js');
-       mix.copy('resources/assets/js/jquery.bootgrid.min.js', 'public/js/jquery.bootgrid.min.js');
-       mix.copy('resources/assets/js/autosize.min.js', 'public/js/autosize.min.js');
-       mix.copy('resources/assets/js/lightgallery.min.js', 'public/js/lightgallery.min.js');
-       mix.copy('resources/assets/js/sweet-alert.min.js', 'public/js/sweet-alert.min.js');
-       mix.copy('resources/assets/js/waves.js', 'public/js/waves.js');
-       mix.copy('resources/assets/js/jquery.mCustomScrollbar.concat.min.js', 'public/js/jquery.mCustomScrollbar.concat.min.js');
-       mix.copy('resources/assets/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
-       mix.copy('resources/assets/js/jquery.min.js', 'public/js/jquery.min.js');
-       mix.copy('resources/assets/js/functions.js', 'public/js/functions.js');
-       mix.copy('resources/assets/js/bootstrap-growl.min.js', 'public/js/bootstrap-growl.min.js');
-       mix.copy('resources/assets/js/fileinput.min.js', 'public/js/fileinput.min.js');
-       mix.copy('resources/assets/js/bootstrap-datetimepicker.min.js', 'public/js/bootstrap-datetimepicker.min.js');
+    // Vendor JS Files
+    mix.scripts([
+        'jquery.min.js',
+        'bootstrap.min.js',
+        'moment.min.js',
+        'simplemde.min.js',
+        'autosize.min.js',
+        'bootstrap-select.js',
+        'jquery.mask.min.js',
+        'chosen.jquery.min.js',
+        'jquery.bootgrid.min.js',
+        'lightgallery.min.js',
+        'sweet-alert.min.js',
+        'waves.js',
+        'jsvalidation.js',
+        'jquery.mCustomScrollbar.concat.min.js',
+        'fileinput.min.js',
+        'bootstrap-datetimepicker.min.js'
+    ], 'public/js/vendor.js');
+
+    // Application JS Files
+    mix.scripts([
+        'functions.js',
+        'bootstrap-growl.min.js',
+    ], 'public/js/app.js');
 
 });
