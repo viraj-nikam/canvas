6288f4c933206e0223b60fe113f40843b1381522
Todd Austin <austin.todd.j@gmail.com>
2016-08-15 12:15:03 -0500

    Adds news story

--- resources/views/backend/home/sections/canvas-news.blade.php
+++ resources/views/backend/home/sections/canvas-news.blade.php
@@ -10,7 +10,31 @@
                     <h4 class="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
                                 aria-expanded="true" aria-controls="collapseOne">
-                            <span class="c-blue">Canvas v2.1.2 Released</span>
+                            <span class="c-blue">Canvas Update v2.1.3</span>
+                            <small>August 15, 2016</small>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="collapse" role="tabpanel"
+                        aria-labelledby="headingOne">
+                    <div class="panel-body">
+                        V2.1.3 gives blog owners a nice landing page to call home. For the newly initiated, this page
+                        will give quick links to common actions in the administrative sections. To the pro-bloggers out
+                        there, quickly jot down a blog draft and save it on the fly. You also get a nice overview
+                        of the status of the site, from Disqus and Google Analytics to Maintenance Mode and the number
+                        of posts or tags you might have.
+                        Check out the
+                        <a href="https://github.com/austintoddj/canvas/releases/tag/v2.1.3" target="_blank">release
+                            notes</a> for a deeper dive into the newest iteration of Canvas!
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-collapse">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
+                                aria-expanded="true" aria-controls="collapseOne">
+                            <span class="c-blue">Canvas Update v2.1.2</span>
                             <small>August 12, 2016</small>
                         </a>
                     </h4>
@@ -18,7 +42,7 @@
                 <div id="collapseOne" class="collapse" role="tabpanel"
                         aria-labelledby="headingOne">
                     <div class="panel-body">
-                        V2.1.2 introduces the Admin Tools section. This release features the ability to bring your site
+                        V2.1.2 introduces the Admin Tools section. This update features the ability to bring your site
                         up or down with Maintenance Mode, export all your blog data in a downloadable archive and more.
                         Check out the
                         <a href="https://github.com/austintoddj/canvas/releases/tag/v2.1.2" target="_blank">release
