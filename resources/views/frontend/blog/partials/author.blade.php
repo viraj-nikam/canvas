26ec94b11ab3e43832735de4f5ea4e917a0e596b
Todd Austin <austin.todd.j@gmail.com>
2016-08-16 22:30:08 -0500

    Cleans up post author signature

--- resources/views/frontend/blog/partials/author.blade.php
+++ resources/views/frontend/blog/partials/author.blade.php
@@ -1,11 +1,23 @@
-<div class="author">
-    <img class="img-responsive img-circle author-img" src="//www.gravatar.com/avatar/{{ md5($user->email) }}?d=identicon&s=150">
-    <h4>{{ $user->first_name .  ' ' . $user->last_name}}
-        <br>
-        <span class="small text-muted author-bio">{{ $user->bio }}</span>
-        <br>
-        <a href="http://github.com/{{ $user->github }}" target="_blank"><i class="fa fa-fw fa-github author-social"></i></a>
-        <a href="http://twitter.com/{{ $user->twitter }}" target="_blank"><i class="fa fa-fw fa-twitter author-social"></i></a>
-        <a href="http://facebook.com/{{ $user->facebook }}" target="_blank"><i class="fa fa-fw fa-facebook author-social"></i></a>
-    </h4>
-</div>
+@if(!empty($user->github) || !empty($user->twitter) || !empty($user->facebook))
+    <div class="author">
+        <img class="img-responsive img-circle author-img" src="//www.gravatar.com/avatar/{{ md5($user->email) }}?d=identicon&s=150">
+        <h5><strong>{{ $user->first_name .  ' ' . $user->last_name }}</strong>
+            <br>
+            <br>
+            <span class="text-muted author-bio">{{ $user->bio }}</span>
+            <br>
+            <br>
+            @if (!empty($user->github))
+                <a href="http://github.com/{{ $user->github }}" target="_blank"><i class="fa fa-fw fa-github author-social"></i></a>
+            @endif
+            &nbsp;
+            @if (!empty($user->twitter))
+                <a href="http://twitter.com/{{ $user->twitter }}" target="_blank"><i class="fa fa-fw fa-twitter author-social"></i></a>
+            @endif
+            &nbsp;
+            @if (!empty($user->facebook))
+                <a href="http://facebook.com/{{ $user->facebook }}" target="_blank"><i class="fa fa-fw fa-facebook author-social"></i></a>
+            @endif
+        </h5>
+    </div>
+@endif
