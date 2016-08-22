f7e7d9dbd0d864f333356c196dd17ededae1d98e
Todd Austin <austin.todd.j@gmail.com>
2016-08-22 10:54:39 -0500

    Routes cleanup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,10 +4,12 @@
 | Canvas Application Routes : Frontend
 |--------------------------------------------------------------------------
 */
+// Blog Index Page
 Route::get('/', 'Frontend\BlogController@index');
 Route::get('blog', 'Frontend\BlogController@index');
-Route::get('blog/{slug}', 'Frontend\BlogController@showPost');
 
+// Blog Post Page
+Route::get('blog/{slug}', 'Frontend\BlogController@showPost');
 /*
 |--------------------------------------------------------------------------
 | Canvas Application Routes : Backend
@@ -17,17 +19,30 @@ $router->group([
     'namespace'  => 'Backend',
     'middleware' => 'auth',
 ], function () {
+    // Home Page
     Route::get('admin', 'HomeController@index');
+
+    // Posts Page
     Route::resource('admin/post', 'PostController', ['except' => 'show']);
+
+    // Tags Page
     Route::resource('admin/tag', 'TagController', ['except' => 'show']);
+
+    // Uploads Page
     Route::get('admin/upload', 'UploadController@index')->name('admin/upload');
     Route::post('admin/upload/file', 'UploadController@uploadFile');
     Route::delete('admin/upload/file', 'UploadController@deleteFile');
     Route::post('admin/upload/folder', 'UploadController@createFolder');
     Route::delete('admin/upload/folder', 'UploadController@deleteFolder');
+
+    // Profile Pages
     Route::get('admin/profile/privacy', 'ProfileController@editPrivacy')->name('admin.profile.privacy');
     Route::resource('admin/profile', 'ProfileController');
+
+    // Search Page
     Route::resource('admin/search', 'SearchController');
+
+    // Tools Page
     Route::get('admin/tools', 'ToolsController@index');
     Route::post('admin/tools/reset_index', 'ToolsController@resetIndex');
     Route::post('admin/tools/cache_clear', 'ToolsController@clearCache');
@@ -45,8 +60,13 @@ $router->group([
     'namespace' => 'Auth',
     'prefix'    => 'auth',
 ], function () {
+    // Login
     Route::get('login', 'AuthController@getLogin');
     Route::post('login', 'AuthController@postLogin');
+
+    // Logout
     Route::get('logout', 'AuthController@getLogout');
+
+    // Passwords
     Route::post('password', 'PasswordController@updatePassword');
 });
