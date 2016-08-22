223a18fa946790615a6697906cbbaac648053628
Todd Austin <austin.todd.j@gmail.com>
2016-08-22 11:29:15 -0500

    Cleans up auth views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,21 +7,22 @@
 @section('login')
     <section id="main">
         <section id="content">
-            <div class="col-md-4 col-md-offset-4">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12">
                 <div class="card">
                     <br>
-                    <div class="card-header">
-                        <center><img src="{{ asset('images/canvas-logo.gif') }}" style="width: 120px"></center>
+                    <div class="card-header" style="text-align: center">
+                        <img src="{{ asset('images/favicon.png') }}" style="width: 85px">
                     </div>
 
                     <div class="card-body card-padding" id="login-ch">
-                        <p class="f-20 f-300 text-center">Please sign in to continue</p>
+                        <p class="f-20 f-300 text-center">Welcome back!</p>
+                        <p class="text-muted text-center">Enter your email address to sign in</p>
 
-                        @include('auth.partials.login-form')
+                        @include('auth.partials.form')
                         <br>
                     </div>
                 </div>
-                <p class="text-center"><a href="/"><i class="zmdi zmdi-long-arrow-return"></i> Go back home</a></p>
+                <p class="text-center"><a href="/"><i class="zmdi zmdi-long-arrow-return"></i> Back to the blog</a></p>
             </div>
         </section>
     </section>
