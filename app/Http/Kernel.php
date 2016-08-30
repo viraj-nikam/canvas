dd91dc722d305f2a9e5df321c52a2aa3a7c16a68
Laravel Shift <shift@laravelshift.com>
2016-08-29 21:18:29 -0400

    Shift Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
     ];
