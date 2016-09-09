54b231dd81504cd318ff59d92a181daaf661b6b6
Todd Austin <taustin@taustinMBP.local>
2016-09-09 15:39:30 -0500

    Code cleanup

--- resources/views/backend/shared/components/show-password.blade.php
+++ resources/views/backend/shared/components/show-password.blade.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-    a.show-password{
+    a.show-password {
         color: #2196f3;
         font-size: 0.9em;
         margin-top: 10px;
@@ -8,33 +8,32 @@
         top: 0;
         transition: all .8s linear;
     }
-    a.show-password:hover{
+
+    a.show-password:hover {
         transition: all .8s linear;
     }
 </style>
-
 <script>
-        (function(){
-            $('input[name="password"], input[name="password_confirmation"], input[name="new_password"], input[name="new_password_confirmation"]').parent().append('<a href="" class="show-password" tabindex="-1"><i class="zmdi zmdi-eye"></i></a>');
-
-            function toggleIcon (elem) {
-                if ( elem.hasClass ( 'zmdi-eye' ) ) {
-                    return elem.removeClass ( 'zmdi-eye' ).addClass ( 'zmdi-eye-off' );
-                }
-                return elem.removeClass ( 'zmdi-eye-off' ).addClass ( 'zmdi-eye' );
+    (function () {
+        $('input[name="password"], input[name="password_confirmation"], input[name="new_password"], input[name="new_password_confirmation"]').parent().append('<a href="" class="show-password" tabindex="-1"><i class="zmdi zmdi-eye"></i></a>');
+        function toggleIcon(elem) {
+            if (elem.hasClass('zmdi-eye')) {
+                return elem.removeClass('zmdi-eye').addClass('zmdi-eye-off');
             }
+            return elem.removeClass('zmdi-eye-off').addClass('zmdi-eye');
+        }
 
-            function toggleType (elem) {
-                return (elem.attr ( 'type' ) === 'password') ? 'text' : 'password';
-            }
+        function toggleType(elem) {
+            return (elem.attr('type') === 'password') ? 'text' : 'password';
+        }
 
-            $('body').on('click', '.show-password', function (e) {
-                var password_field = $(this).siblings('input');
-                var new_type = toggleType( password_field );
-                var current_icon = $(this).children('i');
-                toggleIcon(current_icon);
-                password_field.attr('type', new_type);
-                e.preventDefault();
-            });
-        })();
+        $('body').on('click', '.show-password', function (e) {
+            var password_field = $(this).siblings('input');
+            var new_type = toggleType(password_field);
+            var current_icon = $(this).children('i');
+            toggleIcon(current_icon);
+            password_field.attr('type', new_type);
+            e.preventDefault();
+        });
+    })();
 </script>
--- resources/views/backend/shared/notifications/protip.blade.php
+++ resources/views/backend/shared/notifications/protip.blade.php
@@ -1,10 +1,10 @@
 <script type="text/javascript">
     $(document).ready(function () {
         $(window).load(function () {
-            function notify(message, type){
+            function notify(message, type) {
                 $.growl({
                     message: message
-                },{
+                }, {
                     type: type,
                     allow_dismiss: false,
                     label: 'Cancel',
@@ -24,6 +24,7 @@
                     }
                 });
             }
+
             setTimeout(function () {
                 var message1 = '<strong>ProTip!</strong> Use &#8984;+s or CTL+s as keyboard shortcuts to save a form.';
                 var message2 = '<strong>ProTip!</strong> Use &#8984;+\' to toggle blockquotes in the markdown editor.';
@@ -37,22 +38,20 @@
                 var message10 = '<strong>ProTip!</strong> Use &#8984;+Alt-l to toggle an ordered list in the markdown editor.';
                 var message11 = '<strong>ProTip!</strong> Use F9 to toggle the side by side view markdown editor.';
                 var message12 = '<strong>ProTip!</strong> Use F11 to toggle the fullscreen view of the markdown editor.';
-
                 var options = [
-                                message1,
-                                message2,
-                                message3,
-                                message4,
-                                message5,
-                                message6,
-                                message7,
-                                message8,
-                                message9,
-                                message10,
-                                message11,
-                                message12
-                            ];
-
+                    message1,
+                    message2,
+                    message3,
+                    message4,
+                    message5,
+                    message6,
+                    message7,
+                    message8,
+                    message9,
+                    message10,
+                    message11,
+                    message12
+                ];
                 notify(options[Math.floor(Math.random() * options.length)], 'inverse');
             }, 300)
         });
