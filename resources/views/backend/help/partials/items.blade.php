1a4b337de95ae72cf0af5805a9a7810507efb042
Todd Austin <austin.todd.j@gmail.com>
2016-09-20 11:10:53 -0500

    Adds help section for tags

--- resources/views/backend/help/partials/items.blade.php
+++ resources/views/backend/help/partials/items.blade.php
@@ -14,6 +14,29 @@
     post to update its contents or the <span class="zmdi zmdi-search text-primary"></span> icon to see what it looks like to
     your readers.
 </p>
+<p>
+    To delete a post, go to the <a href="{{ url('admin/posts') }}">Posts</a> page and click the <span class="zmdi zmdi-edit text-primary"></span> action on a specific post.
+    On the following page, click <span class="label label-danger"><span class="zmdi zmdi-delete"></span> Delete</span> to completely remove the post from the blog.
+</p>
+<br>
+
+<h2 class="help-topic-header" id="tags">Tags</h2>
+<p>
+    Tags provide grouping and organization to your blog posts. You can create a new tag by visiting the <span class="zmdi zmdi-labels"></span> Tags
+    menu item in the sidebar, and then clicking the <span class="zmdi zmdi-plus-circle text-primary"></span> icon on the <a href="{{ url('admin/tags') }}">Tags</a> page.
+    Once you specify the name, title and subtitle, click <span class="label label-primary"><span class="zmdi zmdi-floppy"></span> Save</span> to
+    store your new tag in the database.
+</p>
+<p>
+    Assigning a tag to a specific blog post is easy. Just go to the <a href="{{ url('admin/posts') }}">Posts</a> page and click the <span class="zmdi zmdi-edit text-primary"></span> action
+    of whichever post you want to assign the tag to. Within the form, click <code>Tags</code> and select the name of the tag you just created. Once you click
+    <span class="label label-primary"><span class="zmdi zmdi-floppy"></span> Save</span> at the bottom of the page, the blog post will be
+    grouped under that specific tag.
+</p>
+<p>
+    To delete a tag, go to the <a href="{{ url('admin/tags') }}">Tags</a> page and click the <span class="zmdi zmdi-edit text-primary"></span> action on a specific tag.
+    On the following page, click <span class="label label-danger"><span class="zmdi zmdi-delete"></span> Delete</span> to completely remove the tag from the blog.
+</p>
 <br>
 
 <h2 class="help-topic-header">More topics coming soon!</h2>
--- resources/views/backend/help/partials/topics.blade.php
+++ resources/views/backend/help/partials/topics.blade.php
@@ -3,6 +3,7 @@
         <h5>Content</h5>
         <ul id="help-intro">
             <li><a href="#posts">Posts</a></li>
+            <li><a href="#tags">Tags</a></li>
         </ul>
         <br>
     </div>
