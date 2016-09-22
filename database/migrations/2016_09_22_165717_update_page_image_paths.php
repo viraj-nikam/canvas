31faf9947e69a1008e23a335c674a0ceb6f903ed
talvbansal <talvbansal@hotmail.com>
2016-09-22 17:02:19 +0100

    Added image path update migratation

---
+++ database/migrations/2016_09_22_165717_update_page_image_paths.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePageImagePaths extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        \App\Models\Post::chunk(20, function( \Illuminate\Support\Collection $posts){
+            $posts->each(function( \App\Models\Post $post ){
+                if( ! starts_with($post->page_image, '/storage/' ) )
+                {
+                    $post->page_image = '/storage/' . $post->page_image;
+                    $post->save();
+                }
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \App\Models\Post::chunk(20, function( \Illuminate\Support\Collection $posts){
+            $posts->each(function( \App\Models\Post $post ){
+                if( starts_with($post->page_image, '/storage/' ) )
+                {
+                    $post->page_image = str_replace('/storage/', '', $post->page_image);
+                    $post->save();
+                }
+            });
+        });
+    }
+}
