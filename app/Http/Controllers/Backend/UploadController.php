5c400e666393386a6229c924cec426c9f0bd6148
talvbansal <talvbansal@hotmail.com>
2016-09-22 17:17:46 +0100

    more style ci fixes

--- app/Http/Controllers/Backend/UploadController.php
+++ app/Http/Controllers/Backend/UploadController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 
 class UploadController extends Controller
 {
-
     /**
      * Show page of files / subfolders.
      *
--- database/migrations/2016_09_22_165717_update_page_image_paths.php
+++ database/migrations/2016_09_22_165717_update_page_image_paths.php
@@ -11,9 +11,9 @@ class UpdatePageImagePaths extends Migration
      */
     public function up()
     {
-        \App\Models\Post::chunk(20, function( \Illuminate\Support\Collection $posts) {
-            $posts->each(function( \App\Models\Post $post ) {
-                if(! starts_with($post->page_image, '/storage/')) {
+        \App\Models\Post::chunk(20, function (\Illuminate\Support\Collection $posts) {
+            $posts->each(function (\App\Models\Post $post) {
+                if (! starts_with($post->page_image, '/storage/')) {
                     $post->page_image = '/storage/'.$post->page_image;
                     $post->save();
                 }
@@ -28,9 +28,9 @@ class UpdatePageImagePaths extends Migration
      */
     public function down()
     {
-        \App\Models\Post::chunk(20, function( \Illuminate\Support\Collection $posts) {
-            $posts->each(function( \App\Models\Post $post ) {
-                if(starts_with($post->page_image, '/storage/')) {
+        \App\Models\Post::chunk(20, function (\Illuminate\Support\Collection $posts) {
+            $posts->each(function (\App\Models\Post $post) {
+                if (starts_with($post->page_image, '/storage/')) {
                     $post->page_image = str_replace('/storage/', '', $post->page_image);
                     $post->save();
                 }
