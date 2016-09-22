11fd6fd83cb0a80d8157fc173325ed9e6391c6db
talvbansal <talvbansal@hotmail.com>
2016-09-22 17:16:23 +0100

    more style ci fixes

--- database/migrations/2016_09_22_165717_update_page_image_paths.php
+++ database/migrations/2016_09_22_165717_update_page_image_paths.php
@@ -13,8 +13,8 @@ class UpdatePageImagePaths extends Migration
     {
         \App\Models\Post::chunk(20, function( \Illuminate\Support\Collection $posts) {
             $posts->each(function( \App\Models\Post $post ) {
-                if( ! starts_with($post->page_image, '/storage/' ) ) {
-                    $post->page_image = '/storage/' . $post->page_image;
+                if(! starts_with($post->page_image, '/storage/')) {
+                    $post->page_image = '/storage/'.$post->page_image;
                     $post->save();
                 }
             });
@@ -30,7 +30,7 @@ class UpdatePageImagePaths extends Migration
     {
         \App\Models\Post::chunk(20, function( \Illuminate\Support\Collection $posts) {
             $posts->each(function( \App\Models\Post $post ) {
-                if( starts_with($post->page_image, '/storage/' ) ) {
+                if(starts_with($post->page_image, '/storage/')) {
                     $post->page_image = str_replace('/storage/', '', $post->page_image);
                     $post->save();
                 }
