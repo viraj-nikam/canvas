9f8553346c46566dd3dd109b4f953a886bfe4f16
Todd Austin <austintoddj@users.noreply.github.com>
2016-09-27 06:39:13 -0500

    Adds breadcrumbs to media library

--- resources/views/backend/upload/index.blade.php
+++ resources/views/backend/upload/index.blade.php
@@ -11,8 +11,25 @@
             <div class="container">
                 <div class="card">
                     <div class="card-header">
+                        <ol class="breadcrumb">
+                            <li><a href="{{ url('admin') }}">Home</a></li>
+                            <li class="active">Media</li>
+                        </ol>
+                        <ul class="actions">
+                            <li class="dropdown">
+                                <a href="" data-toggle="dropdown">
+                                    <i class="zmdi zmdi-more-vert"></i>
+                                </a>
+
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li>
+                                        <a href="">Refresh Media Library</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
                         <h2>Media Library
-                            <small>This page provides a comprehensive overview of your media library.</small>
+                            <small>All the files you’ve uploaded are listed alphabetically in the Media Library. Double-click a folder name to see its contents.</small>
                         </h2>
                     </div>
 
