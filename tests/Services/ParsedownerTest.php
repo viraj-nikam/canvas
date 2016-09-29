4e52e356c04041f5b247ae42856e45cc92349ef8
Todd Austin <austin.todd.j@gmail.com>
2016-09-29 13:43:11 -0500

    Cleans up Parsedowner tests

--- tests/Services/ParsedownerTest.php
+++ tests/Services/ParsedownerTest.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Services\Parsedowner;
+
 class ParsedownerTest extends PHPUnit_Framework_TestCase
 {
     protected $parsedowner;
 
     public function setup()
     {
-        $this->parsedowner = new \App\Services\Parsedowner();
+        $this->parsedowner = new Parsedowner();
     }
 
     /**
@@ -31,7 +33,9 @@ class ParsedownerTest extends PHPUnit_Framework_TestCase
             ['`hello_world`', '<p><code>hello_world</code></p>'],
             ['``` <?php $var = "Php Code Blocks"; echo $var; ?> ```', '<p><code>&lt;?php $var = "Php Code Blocks"; echo $var; ?&gt;</code></p>'],
             ['*italics text*', '<p><em>italics text</em></p>'],
+            ['_italics text_', '<p><em>italics text</em></p>'],
             ['**bold text**', '<p><strong>bold text</strong></p>'],
+            ['__bold text__', '<p><strong>bold text</strong></p>'],
             ['---', '<hr />'],
             ['***', '<hr />'],
             ['>note', "<blockquote>\n<p>note</p>\n</blockquote>"],
@@ -41,6 +45,7 @@ class ParsedownerTest extends PHPUnit_Framework_TestCase
             ['![Canvas Logo](http://canvas.toddaustin.io/img/canvas-logo.gif)', '<p><img src="http://canvas.toddaustin.io/img/canvas-logo.gif" alt="Canvas Logo" /></p>'],
             ['- List Item', "<ul>\n<li>List Item</li>\n</ul>"],
             ['1. List Item', "<ol>\n<li>List Item</li>\n</ol>"],
+            ['[Canvas](http://canvas.toddaustin.io)', '<p><a href="http://canvas.toddaustin.io">Canvas</a></p>'],
         ];
     }
 }
