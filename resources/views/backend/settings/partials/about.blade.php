51879dd01979aa538cc9d2bf33676d0eb1f09920
Todd Austin <austin.todd.j@gmail.com>
2016-10-07 14:34:32 -0500

    Tweaks to the about section

--- resources/views/backend/settings/partials/about.blade.php
+++ resources/views/backend/settings/partials/about.blade.php
@@ -1,14 +1,13 @@
 <div class="card">
     <div class="card-header">
         <h2>About Canvas
-            <small>General information and credits</small>
+            <small>General information and credits.</small>
         </h2>
     </div>
     <div class="card-body card-padding">
         <br>
-        <p>Canvas was born out of a desire for an elegant, well-architectured personal publishing platform built on <a href="https://laravel.com/" target="_blank">Laravel</a> that would be
-           attractive to developers and non-developers alike.</p>
-        <p>Canvas is a simple, powerful blog publishing platform that lets you to share your stories with the world. Its beautifully designed interface and completely customizable framework allows you to create and publish your own blog, giving you tools that make it easy and even fun to do.</p>
+        <p><a href="http://canvas.toddaustin.io" target="_blank">Canvas</a> was born out of a desire for an elegant, well-architectured personal publishing platform built on <a href="https://laravel.com/" target="_blank">Laravel</a> that would be
+           attractive to developers and non-developers alike. If you like using this application and feel like it has helped you in any way, share it, tweet about it, fork it and give it a star on <a href="https://github.com/austintoddj/canvas" target="_blank">GitHub</a>.</p>
         <hr>
         <div id="about">
             <p id="social">
