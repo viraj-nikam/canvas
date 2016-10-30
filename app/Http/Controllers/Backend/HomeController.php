ef21d9558aee1fdf43710776e66ec258b2a8cc30
Todd Austin <austin.todd.j@gmail.com>
2016-10-30 18:21:18 -0500

    View error log

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -17,6 +17,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        dd(env('APP_ENV'));
         $data = [
             'posts' => Post::all(),
             'recentPosts' => Post::orderBy('created_at', 'desc')->take(4)->get(),
