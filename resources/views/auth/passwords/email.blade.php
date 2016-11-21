97f463084ece6e8f04f886e6260489da8a1a518a
Todd Austin <austin.todd.j@gmail.com>
2016-11-21 10:44:59 -0600

    Cleans up auth form pages

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,22 +5,9 @@
 @stop
 
 @section('login')
-    <div class="contain">
-        <section id="main">
-            <section id="content">
-                <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12">
-                    <div class="card">
-                        <div class="card-body card-padding" id="login-ch">
-                            <p class="f-20 f-300">Forgot your password?</p>
-                            @include('shared.errors')
-                            @include('auth.passwords.partials.email-form')
-                        </div>
-                    </div>
-                    <p class="text-center"><a href="/"><i class="zmdi zmdi-long-arrow-return"></i> Back to the blog</a>
-                    </p>
-                </div>
-            </section>
-        </section>
+    <div class="container container-fluid login-container">
+        @include('shared.errors')
+        @include('auth.passwords.partials.email-form')
     </div>
 @endsection
 
