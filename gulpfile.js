ba275ae630f45f157c450fd9e1d0f729c914709f
Todd Austin <austin.todd.j@gmail.com>
2017-01-18 10:56:09 -0600

    Test cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,4 @@ require('laravel-elixir-vue-2');
 
 elixir(function (mix) {
     // Your gulp tasks!
-    
-    // Run unit tests and generate reports each time Gulp is run
-    // mix.phpUnit();
 });
