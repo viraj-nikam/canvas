b0af33036af9d7dfdcdd98fc9cdede7b0b0b6cc4
Todd Austin <austin.todd.j@gmail.com>
2017-08-21 11:31:52 -0500

    Refactor backend route testing

--- tests/Feature/Routes/BackendRoutesTest.php
+++ tests/Feature/Routes/BackendRoutesTest.php
@@ -11,115 +11,37 @@ class AdminRoutesTest extends TestCase
 {
     use InteractsWithDatabase, CreatesUser;
 
-    /** @test */
-    public function it_can_access_the_home_page()
+    /**
+    * Smoke test each URI and compare the response codes.
+    *
+    * @test
+    *
+    * @dataProvider uriWithResponseCodeProvider
+    **/
+    public function it_gets_proper_response_codes_from_backend_uris($uri, $responseCode)
     {
         Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin'));
-        $response->assertStatus(200);
+        $response = $this->actingAs(Auth::user())->call('GET', $uri);
+        $this->assertEquals($responseCode, $response->status());
     }
 
-    /** @test */
-    public function it_can_access_the_posts_index_page()
+    public function uriWithResponseCodeProvider()
     {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.post.index'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_edit_posts_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.post.edit', 1));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_tags_index_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.tag.index'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_edit_tags_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.tag.edit', 1));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_media_library_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.upload'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_profile_index_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.profile.index'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_profile_privacy_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.profile.privacy'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_tools_index_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.tools'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_settings_index_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.settings'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_help_index_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.help'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_users_index_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.user.index'));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_edit_users_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.user.edit', 2));
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function it_can_access_the_edit_users_privacy_page()
-    {
-        Auth::guard('canvas')->login($this->user);
-        $response = $this->actingAs(Auth::user())->call('GET', route('canvas.admin.user.privacy', 2));
-        $response->assertStatus(200);
+        return [
+            [route('canvas.admin'), 200],
+            [route('canvas.admin.post.index'), 200],
+            [route('canvas.admin.post.edit', 1, 200],
+            [route('canvas.admin.tag.index'), 200],
+            [route('canvas.admin.tag.edit', 1, 200],
+            [route('canvas.admin.upload'), 200],
+            [route('canvas.admin.profile.index'), 200],
+            [route('canvas.admin.profile.privacy'), 200],
+            [route('canvas.admin.tools'), 200],
+            [route('canvas.admin.settings'), 200],
+            [route('canvas.admin.help'), 200],
+            [route('canvas.admin.user.index'), 200],
+            [route('canvas.admin.user.edit', 2, 200],
+            [route('canvas.admin.user.privacy', 2), 200],
+        ];
     }
 }
