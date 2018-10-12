6e1f4cae4c2db4c5ff6f9999a553edb76d5f9f8c
Todd Austin <austintoddj@users.noreply.github.com>
2018-10-11 20:10:06 -0500

    Apply fixes from StyleCI (#378)

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('Canvas\Http\Controllers')->group(function () {
-
     Route::middleware(['web'])->prefix('blog')->group(function () {
         // Homepage Routes...
         Route::get('/', 'HomeController@index')->name('canvas.public.index');
     });
 
-    Route::middleware(['web','auth'])->prefix('canvas')->group(function () {
+    Route::middleware(['web', 'auth'])->prefix('canvas')->group(function () {
         // Dashboard Routes...
         Route::get('/', 'DashboardController@index')->name('canvas.admin.index');
     });
-
 });
