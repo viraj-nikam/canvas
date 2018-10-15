52c8cc96c240658d8a87c678086cf1e0a53c23a7
Todd Austin <austin.todd.j@gmail.com>
2018-10-15 18:47:23 -0500

    Added unit tests and asset fixes

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -4,6 +4,7 @@ namespace Canvas\Tests\Unit;
 
 use Canvas\Entities\Post;
 use Canvas\Tests\TestCase;
+use Canvas\Interfaces\PostInterface;
 
 class PostTest extends TestCase
 {
@@ -26,8 +27,17 @@ class PostTest extends TestCase
     /** @test */
     public function it_is_retrievable_by_id()
     {
-        $post_by_id = app(Post::class)->find($this->testPost->id);
+        $post_by_id = Post::find($this->testPost->id);
 
         $this->assertEquals($this->testPost->id, $post_by_id->id);
+    }
+
+    /** @test */
+    public function is_can_retrieve_all_posts_for_a_user()
+    {
+        $this->refreshTestPost();
+        Post::create(['title' => 'User Post', 'summary' => '', 'body' => '', 'user_id' => $this->testUser->id, 'published' => 1]);
+
+        $this->assertCount(2, app(PostInterface::class)->getByUserId($this->testUser->id));
     }
 }
