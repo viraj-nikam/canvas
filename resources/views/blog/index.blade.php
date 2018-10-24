73b2a568787bfdc51c65baf200c6ae706e6a0e16
Todd Austin <austin.todd.j@gmail.com>
2018-10-23 22:02:14 -0500

    Added verbiage for no posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,16 +6,23 @@
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col col-lg-8">
-                @foreach($data['posts'] as $post)
-                    <p class="lead"><a href="{{ route('blog.post.show', $post->slug) }}">{{ $post->title }}</a></p>
-                    <p>{{ $post->summary }}</p>
-                    @if($post->tags->count() > 0)
-                        @foreach($post->tags as $tag)
-                            <p><a href="{{ route('blog.tag.show', $tag->slug) }}" class="badge badge-pill badge-primary">{{ $tag->name }}</a></p>
-                        @endforeach
-                    @endif
-                    <hr>
-                @endforeach
+                @if(count($data['posts']) > 0)
+                    @foreach($data['posts'] as $post)
+                        <p class="lead"><a href="{{ route('blog.post.show', $post->slug) }}">{{ $post->title }}</a></p>
+                        <p>{{ $post->summary }}</p>
+                        @if($post->tags->count() > 0)
+                            @foreach($post->tags as $tag)
+                                <p><a href="{{ route('blog.tag.show', $tag->slug) }}" class="badge badge-pill badge-primary">{{ $tag->name }}</a></p>
+                            @endforeach
+                        @endif
+                        <hr>
+                    @endforeach
+                @else
+                    <div class="text-center">
+                        <p class="lead mt-5 mb-4">Nothing much to see here yet, but you can change that.</p>
+                        <a href="{{ route('canvas.post.create') }}" class="btn btn-outline-primary mx-auto">New Post</a>
+                    </div>
+                @endif
             </div>
         </div>
         <div class="row mx-auto justify-content-center">
