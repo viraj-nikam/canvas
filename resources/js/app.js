baa01c3c48d69ed3dd00ed62922b0403e3e79a3c
Todd Austin <austin.todd.j@gmail.com>
2018-11-01 10:50:42 -0500

    Asset updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,53 +1,22 @@
 
-window._ = require('lodash');
-window.Popper = require('popper.js').default;
-
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
+require('./bootstrap');
 
-    require('bootstrap');
-} catch (e) {}
+window.Vue = require('vue');
 
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-window.axios = require('axios');
+Vue.component('text-editor', require('./components/TextEditor.vue'));
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-require('./bootstrap/alert');
-require('./bootstrap/carousel');
-require('./bootstrap/collapse');
-require('./bootstrap/dropdown');
-require('./bootstrap/modal');
-require('./bootstrap/popover');
-require('./bootstrap/scrollspy');
-require('./bootstrap/tab');
-require('./bootstrap/tooltip');
-require('./bootstrap/util');
-require('./custom/affix');
-require('./custom/datepicker');
-require('./custom/spinner');
+const app = new Vue({
+    el: '#app'
+});
--- resources/views/canvas/index.blade.php
+++ resources/views/canvas/index.blade.php
@@ -13,15 +13,14 @@
 @endpush
 
 @section('body')
-    @include('canvas::canvas.components.notifications.success')
-    @include('canvas::canvas.components.notifications.error')
-
     <div class="container">
         <div class="row">
             <div class="col-md-3 sidebar">
                 @include('canvas::canvas.components.nav.sidebar')
             </div>
             <div class="col-md-9 content">
+                @include('canvas::canvas.components.notifications.success')
+                @include('canvas::canvas.components.notifications.error')
                 @yield('content')
             </div>
         </div>
