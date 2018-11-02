f5bea8d4c82eb25e46f2edcdae7b13ef84643ec0
Todd Austin <austin.todd.j@gmail.com>
2018-11-02 09:09:16 -0500

    Templating and asset management

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -6,13 +6,7 @@
 
     <title>@yield('title')</title>
 
-    <script src="{{ mix('js/app.js', 'vendor/canvas') }}"></script>
-    <script defer src="{{ url('https://use.fontawesome.com/releases/v5.3.1/js/all.js') }}"
-            integrity="sha384-kW+oWsYx3YpxvjtZjFXqazFpA7UP/MbiY4jvs+RWZo2+N94PFZ36T6TFkc9O3qoB"
-            crossorigin="anonymous"></script>
-
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css', 'vendor/canvas') }}">
-    <link rel="stylesheet" href="{{ url('https://fonts.googleapis.com/css?family=Nunito') }}">
 </head>
 <body>
     @yield('body')
