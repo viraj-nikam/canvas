006980bee47a4a46bb9b554e16421c3af3ec8338
Todd Austin <austin.todd.j@gmail.com>
2018-11-05 07:36:30 -0600

    Added vue-select and opened a bug with script placement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>@yield('title')</title>
-
-    <script type="text/javascript" src="{{ mix('js/app.js', 'vendor/canvas') }}"></script>
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css', 'vendor/canvas') }}">
 </head>
 <body>
-<div id="app">
-    @yield('body')
-</div>
+    <div id="app">
+        @yield('body')
+    </div>
+    <script type="text/javascript" src="{{ mix('js/app.js', 'vendor/canvas') }}"></script>
 </body>
 </html>
