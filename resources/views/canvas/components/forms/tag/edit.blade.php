a46ed47ba7afae7308245bc10898ce448dd1f237
Todd Austin <austin.todd.j@gmail.com>
2018-12-03 16:13:10 -0600

    Fixed the tag inputs

--- resources/views/canvas/components/forms/tag/edit.blade.php
+++ resources/views/canvas/components/forms/tag/edit.blade.php
@@ -4,7 +4,9 @@
 
     <div class="form-group row my-5">
         <div class="col-lg-12">
-            <input type="text" class="form-control-lg form-control{{ $errors->has('name') ? ' is-invalid' : '' }} border-0" name="name"
+            <input type="text"
+                   class="form-control-lg form-control{{ $errors->has('name') ? ' is-invalid' : '' }} border-0 px-0"
+                   name="name"
                    title="Name" value="{{ $data['tag']->name }}" required placeholder="Give your tag a name">
             @if ($errors->has('name'))
                 <div class="invalid-feedback">
@@ -15,7 +17,9 @@
     </div>
     <div class="form-group row">
         <div class="col-lg-12">
-            <input type="text" class="form-control-lg form-control{{ $errors->has('slug') ? ' is-invalid' : '' }} border-0" name="slug"
+            <input type="text"
+                   class="form-control-lg form-control{{ $errors->has('slug') ? ' is-invalid' : '' }} border-0 px-0"
+                   name="slug"
                    title="Slug" value="{{ $data['tag']->slug }}" required placeholder="now-add-a-slug">
             @if ($errors->has('slug'))
                 <div class="invalid-feedback">
