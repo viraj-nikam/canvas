97c511651bfcd96da3d54cf60569937bc75876e1
Todd Austin <austin.todd.j@gmail.com>
2018-12-19 07:12:53 -0600

    Fixing the vue errors

--- resources/js/components/ViewStats.vue
+++ resources/js/components/ViewStats.vue
@@ -21,18 +21,20 @@
 </script>
 
 <template>
-    <vue-frappe
-            id="stats"
-            :labels="[
+    <div>
+        <vue-frappe
+                id="stats"
+                :labels="[
                 '12/01', '12/02', '12/03', '12/04',
                 '12/05', '12/06', '12/07', '12/08',
                 '12/09', '12/10', '12/11', '12/12',
                 '12/13', '12/14', '12/15', '12/16'
             ]"
-            title="Views (Last 30 days)"
-            type="line"
-            :height="250"
-            :colors="['#3490dc']"
-            :dataSets="this.points">
-    </vue-frappe>
+                title="Views (Last 30 days)"
+                type="line"
+                :height="250"
+                :colors="['#3490dc']"
+                :dataSets="this.points">
+        </vue-frappe>
+    </div>
 </template>
