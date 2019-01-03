83846fddf3495654d338cdb68c344a7218207178
Todd Austin <austin.todd.j@gmail.com>
2019-01-03 11:42:24 -0600

    Added moment() manipulation to the line charts

--- resources/js/components/LineChart.vue
+++ resources/js/components/LineChart.vue
@@ -1,19 +1,22 @@
 <script type="text/ecmascript-6">
     import Vue from 'vue'
     import Chart from 'vue2-frappe'
+    import moment from 'moment';
 
     Vue.use(Chart);
+    Vue.prototype.moment = moment;
 
     export default {
         props: ['views'],
 
         data() {
             return {
-                points: [
+                points: [{
+                    values: [15, 20, 35, 33, 47, 32, 29, 34, 15, 20, 365, 33, 84, 32, 29, 35]
+                }],
 
-                    // todo: [task] loop through the views property and grab values
-
-                    {values: [15, 20, 35, 33, 47, 32, 29, 34, 15, 20, 365, 33, 84, 32, 29, 35]}
+                labels: [
+                    '12/01', '12/02', '12/03', '12/04', '12/05', '12/06', '12/07', '12/08', '12/09', '12/10', '12/11', '12/12', '12/13', '12/14', '12/15', '12/16'
                 ]
             }
         }
@@ -24,22 +27,16 @@
     <div>
         <vue-frappe
                 id="stats"
-                :labels="[
-                    '12/01', '12/02', '12/03', '12/04',
-                    '12/05', '12/06', '12/07', '12/08',
-                    '12/09', '12/10', '12/11', '12/12',
-                    '12/13', '12/14', '12/15', '12/16'
-                ]"
+                :labels="this.labels"
                 title="Views (Last 30 days)"
                 type="line"
                 :height="250"
                 :colors="['#3490dc']"
                 :dataSets="this.points"
                 :tooltipOptions="{
-                    formatTooltipX: d => (d + '').toUpperCase(),
+                    formatTooltipX: d => moment(d).format('dddd, MMMM Do'),
                     formatTooltipY: d => d + ' views',
-                }"
-                >
+                }">
         </vue-frappe>
     </div>
 </template>
