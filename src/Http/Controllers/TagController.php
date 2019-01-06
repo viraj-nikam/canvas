b96e6fc48a1d74e512f45a5fd552f489ecd1cf2e
Todd Austin <austintoddj@users.noreply.github.com>
2019-01-06 08:50:49 -0600

    Apply fixes from StyleCI (#418)

--- src/Http/Controllers/TagController.php
+++ src/Http/Controllers/TagController.php
@@ -93,7 +93,7 @@ class TagController extends Controller
 
         validator($data, [
             'name' => 'required',
-            'slug' => 'required|' . Rule::unique('canvas_tags', 'slug')->ignore(request('id')) . '|regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/i',
+            'slug' => 'required|'.Rule::unique('canvas_tags', 'slug')->ignore(request('id')).'|regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/i',
         ])->validate();
 
         $tag = new Tag(['id' => request('id')]);
@@ -121,7 +121,7 @@ class TagController extends Controller
 
         validator($data, [
             'name' => 'required',
-            'slug' => 'required|' . Rule::unique('canvas_tags', 'slug')->ignore(request('id')) . '|regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/i',
+            'slug' => 'required|'.Rule::unique('canvas_tags', 'slug')->ignore(request('id')).'|regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/i',
         ])->validate();
 
         $tag->fill($data);
