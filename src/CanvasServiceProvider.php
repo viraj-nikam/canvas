a5aa2422565373505dc3c598d126c0ecf68d3582
Todd Austin <austin.todd.j@gmail.com>
2019-03-11 20:56:31 -0500

    Apply fixes from StyleCI (#444)

--- src/CanvasServiceProvider.php
+++ src/CanvasServiceProvider.php
@@ -36,7 +36,7 @@ class CanvasServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/canvas.php', 'canvas'
+            __DIR__.'/../config/canvas.php', 'canvas'
         );
 
         $this->commands([
@@ -71,7 +71,7 @@ class CanvasServiceProvider extends ServiceProvider
     {
         Route::namespace('Canvas\Http\Controllers')->group(function () {
             Route::prefix('canvas')->middleware(config('canvas.middleware'))->group(function () {
-                $this->loadRoutesFrom(__DIR__ . '/../routes/canvas.php');
+                $this->loadRoutesFrom(__DIR__.'/../routes/canvas.php');
             });
         });
     }
@@ -83,7 +83,7 @@ class CanvasServiceProvider extends ServiceProvider
      */
     private function registerResources()
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'canvas');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'canvas');
     }
 
     /**
@@ -94,7 +94,7 @@ class CanvasServiceProvider extends ServiceProvider
     private function registerMigrations()
     {
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__ . '/Migrations');
+            $this->loadMigrationsFrom(__DIR__.'/Migrations');
         }
     }
 
@@ -107,10 +107,10 @@ class CanvasServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../public' => public_path('vendor/canvas'),
+                __DIR__.'/../public' => public_path('vendor/canvas'),
             ], 'canvas-assets');
             $this->publishes([
-                __DIR__ . '/../config/canvas.php' => config_path('canvas.php'),
+                __DIR__.'/../config/canvas.php' => config_path('canvas.php'),
             ], 'canvas-config');
         }
     }
