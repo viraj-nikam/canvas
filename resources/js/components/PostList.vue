ea539996fde96c5caedbcaa4ae10ac1a5b3b12fe
Todd Austin <austin.todd.j@gmail.com>
2019-04-05 05:50:10 -0500

    refactored the float parsing for view counts

--- resources/js/components/PostList.vue
+++ resources/js/components/PostList.vue
@@ -26,19 +26,19 @@
                     suffix = '';
                 } else if (n < 900000) {
                     let n_total = n / 1000;
-                    format = n_total.toFixed(1);
+                    format = parseFloat(n_total.toFixed(1));
                     suffix = 'K';
                 } else if (n < 900000000) {
                     let n_total = n / 1000000;
-                    format = n_total.toFixed(1);
+                    format = parseFloat(n_total.toFixed(1));
                     suffix = 'M';
                 } else if (n < 900000000000) {
                     let n_total = n / 1000000000;
-                    format = n_total.toFixed(1);
+                    format = parseFloat(n_total.toFixed(1));
                     suffix = 'B';
                 } else {
                     let n_total = n / 1000000000000;
-                    format = n_total.toFixed(1);
+                    format = parseFloat(n_total.toFixed(1));
                     suffix = 'T';
                 }
 
