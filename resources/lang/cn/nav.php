96e4460d6602d97b12e9a279ed126b3d7fb610e4
Todd Austin <austin.todd.j@gmail.com>
2019-04-26 05:58:53 -0500

    adds support for chinese simplified

---
+++ resources/lang/cn/nav.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'context'  => [
+        'published' => '發布時間',
+        'draft'     => '草案',
+    ],
+    'notify'   => [
+        'success' => '得救了！',
+    ],
+    'user'     => [
+        'posts'  => '帖子',
+        'tags'   => '標籤',
+        'topics' => '話題',
+        'stats'  => '統計',
+        'logout' => '登出',
+    ],
+    'controls' => [
+        'stats'    => '查看統計信息',
+        'settings' => '常規設置',
+        'image'    => '特色圖片',
+        'seo'      => 'SEO和社交',
+    ],
+
+];
