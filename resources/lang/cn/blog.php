c06729a367067658aec50f09814d086b4fb8b2ef
Todd Austin <austin.todd.j@gmail.com>
2019-04-26 06:36:53 -0500

    added multilingual support to the default template

---
+++ resources/lang/cn/blog.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'title'   => '博客',
+    'buttons' => [
+        'learn' => '学到更多',
+        'next'  => '阅读下一篇',
+        'enjoy' => '你可能会喜欢',
+        'edit'  => '编辑帖子',
+        'stats' => '查看统计信息',
+    ],
+    'empty'   => [
+        'description' => '没有找到帖子，从头开始',
+        'action'      => '添加新帖子',
+    ],
+    'nav'     => [
+        'user' => [
+            'posts'  => '帖子',
+            'tags'   => '标签',
+            'topics' => '话题',
+            'stats'  => '统计',
+            'login'  => '登入',
+            'logout' => '登出',
+        ],
+    ],
+    'posts'   => [
+        'label' => '最近的帖子',
+    ],
+    'tags'    => [
+        'label' => '标签',
+    ],
+
+];
---
+++ resources/lang/en/blog.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'title'   => 'Blog',
+    'buttons' => [
+        'learn' => 'Learn More',
+        'next'  => 'Read this next',
+        'enjoy' => 'You might enjoy',
+        'edit'  => 'Edit post',
+        'stats' => 'View stats',
+    ],
+    'empty'   => [
+        'description' => 'No posts were found, start by',
+        'action'      => 'adding a new post',
+    ],
+    'nav'     => [
+        'user' => [
+            'posts'  => 'Posts',
+            'tags'   => 'Tags',
+            'topics' => 'Topics',
+            'stats'  => 'Stats',
+            'login'  => 'Sign in',
+            'logout' => 'Sign out',
+        ],
+    ],
+    'posts'   => [
+        'label' => 'Recent posts',
+    ],
+    'tags'    => [
+        'label' => 'Tags',
+    ],
+
+];
