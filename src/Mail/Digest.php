543398a3a881608b9012d9e541faafd1fef9d512
Todd Austin <austin.todd.j@gmail.com>
2019-06-12 13:40:08 -0500

    minor refactor of the digest email

--- src/Mail/Digest.php
+++ src/Mail/Digest.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class WeeklyDigest extends Mailable
+class Digest extends Mailable
 {
     use Queueable, SerializesModels;
 
