8cb8af47f61c30aacec9dd917fddf36ca2d1a173
Todd Austin <austin.todd.j@gmail.com>
2019-07-16 11:00:32 -0500

    Apply fixes from StyleCI (#530)

--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -89,7 +89,7 @@ class PostController extends Controller
         ];
 
         validator($data, [
-            'slug'         => 'required|' . Rule::unique('canvas_posts', 'slug')->ignore(request('id')) . '|regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/i',
+            'slug'         => 'required|'.Rule::unique('canvas_posts', 'slug')->ignore(request('id')).'|regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/i',
             'published_at' => 'required|date',
             'user_id'      => 'required',
         ], $messages)->validate();
@@ -142,7 +142,7 @@ class PostController extends Controller
         return collect($incomingTags)->map(function ($incomingTag) use ($tags) {
             $tag = $tags->where('slug', $incomingTag['slug'])->first();
 
-            if (!$tag) {
+            if (! $tag) {
                 $tag = Tag::create([
                     'id'   => $id = Uuid::uuid4(),
                     'name' => $incomingTag['name'],
@@ -150,7 +150,7 @@ class PostController extends Controller
                 ]);
             }
 
-            return (string)$tag->id;
+            return (string) $tag->id;
         })->toArray();
     }
 
@@ -166,7 +166,7 @@ class PostController extends Controller
         if ($incomingTopic) {
             $topic = Topic::where('slug', $incomingTopic['slug'])->first();
 
-            if (!$topic) {
+            if (! $topic) {
                 $topic = Topic::create([
                     'id'   => $id = Uuid::uuid4(),
                     'name' => $incomingTopic['name'],
@@ -174,7 +174,7 @@ class PostController extends Controller
                 ]);
             }
 
-            return collect((string)$topic->id)->toArray();
+            return collect((string) $topic->id)->toArray();
         } else {
             return [];
         }
