4540c96a69b9288912b95d3f6a2572b32c623c63
Todd Austin <austin.todd.j@gmail.com>
2019-10-21 12:19:56 -0500

    Apply fixes from StyleCI (#563)

--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -155,7 +155,7 @@ class PostController extends Controller
         if ($incomingTopic) {
             $topic = Topic::where('slug', $incomingTopic['slug'])->first();
 
-            if (!$topic) {
+            if (! $topic) {
                 $topic = Topic::create([
                     'id'   => $id = Uuid::uuid4(),
                     'name' => $incomingTopic['name'],
@@ -163,7 +163,7 @@ class PostController extends Controller
                 ]);
             }
 
-            return collect((string)$topic->id)->toArray();
+            return collect((string) $topic->id)->toArray();
         } else {
             return [];
         }
@@ -183,7 +183,7 @@ class PostController extends Controller
             return collect($incomingTags)->map(function ($incomingTag) use ($tags) {
                 $tag = $tags->where('slug', $incomingTag['slug'])->first();
 
-                if (!$tag) {
+                if (! $tag) {
                     $tag = Tag::create([
                         'id'   => $id = Uuid::uuid4(),
                         'name' => $incomingTag['name'],
@@ -191,7 +191,7 @@ class PostController extends Controller
                     ]);
                 }
 
-                return (string)$tag->id;
+                return (string) $tag->id;
             })->toArray();
         } else {
             return [];
