d3b95b322df2dad382b645e50e2224eeefc0d7cd
Todd Austin <austin.todd.j@gmail.com>
2019-12-04 21:21:54 -0600

    refactored the line chart to a simpler setup

--- resources/js/components/LineChart.vue
+++ resources/js/components/LineChart.vue
@@ -1,32 +1,11 @@
 <template>
-    <div v-cloak>
-        <vue-frappe
-            id="stats"
-            :labels="labels"
-            :title="trans.stats.cards.views.title"
-            type="line"
-            :axisOptions="{
-                xIsSeries: true,
-            }"
-            :lineOptions="{
-                regionFill: 1,
-                hideDots: 1,
-            }"
-            :height="250"
-            :colors="['#03a87c']"
-            :dataSets="points"
-            :tooltipOptions="{
-                formatTooltipX: d => moment(d, 'YYYY-MM-DD').format('dddd, MMMM Do'),
-                formatTooltipY: d => d + plural(trans.stats.chart.view, d),
-            }"
-        />
-    </div>
+    <div id="stats"></div>
 </template>
 
 <script>
     import Vue from 'vue'
     import moment from 'moment'
-    import VueFrappe from 'vue-frappe'
+    import {Chart} from 'frappe-charts/dist/frappe-charts.esm.js'
 
     Vue.prototype.moment = moment
 
@@ -34,7 +13,7 @@
         name: 'line-chart',
 
         components: {
-            VueFrappe,
+            Chart
         },
 
         props: {
@@ -46,13 +25,44 @@
 
         data() {
             return {
-                labels: Object.keys(this.views),
-                points: [{
-                    values: Object.values(this.views),
-                }],
                 trans: JSON.parse(Canvas.lang),
-                isReady: false,
+            }
+        },
+
+        mounted() {
+            this.createChart();
+        },
+
+        methods: {
+            createChart() {
+                return new Chart('#stats', {
+                    data: {
+                        labels: Object.keys(this.views),
+                        datasets: [{
+                            values: Object.values(this.views)
+                        }]
+                    },
+                    title: this.trans.stats.cards.views.title,
+                    type: 'line',
+                    axisOptions: {
+                        xIsSeries: true,
+                    },
+                    lineOptions: {
+                        regionFill: 1,
+                        hideDots: 1,
+                    },
+                    height: 250,
+                    colors: ['#03a87c'],
+                    tooltipOptions: {
+                        formatTooltipX: d => moment(d, 'YYYY-MM-DD').format('dddd, MMMM Do'),
+                        formatTooltipY: d => d + this.plural(this.trans.stats.chart.view, d),
+                    }
+                });
             }
         }
     }
 </script>
+
+<style scoped>
+    @import '~frappe-charts/dist/frappe-charts.min.css';
+</style>
