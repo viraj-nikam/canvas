0ad3f3e136a0d2c32f258a9892eb1a19f8a89df6
Todd Austin <austin.todd.j@gmail.com>
2019-12-11 05:24:07 -0600

    Apply fixes from StyleCI (#579)

--- src/Http/Controllers/SettingsController.php
+++ src/Http/Controllers/SettingsController.php
@@ -2,8 +2,8 @@
 
 namespace Canvas\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class SettingsController extends Controller
