7f9997ad17fc53452f6bcc3e67e8438b26e5068b
Todd Austin <austin.todd.j@gmail.com>
2020-01-26 05:07:09 -0600

    added file removal to the media controller

--- src/Http/Controllers/MediaController.php
+++ src/Http/Controllers/MediaController.php
@@ -8,30 +8,44 @@ use Illuminate\Support\Facades\Storage;
 class MediaController extends Controller
 {
     /**
-     * Store a file upload and return the path.
+     * Stores a given file and returns the path.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return mixed
      */
     public function store()
     {
-        $path = request()->file('upload')->store(sprintf('%s/%s', config('canvas.storage_path'), 'images'), [
-            'disk'       => config('canvas.storage_disk'),
-            'visibility' => 'public',
+        $path = request()->file('upload')->storePublicly($this->baseStoragePath(), [
+            'disk' => config('canvas.storage_disk'),
         ]);
 
-        return response()->json(Storage::disk(config('canvas.storage_disk'))->url($path), 200);
+        return Storage::disk(config('canvas.storage_disk'))->url($path);
     }
 
     /**
-     * Delete a file from storage.
+     * Deletes a given file from storage.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function destroy()
     {
-        // todo: find a way to parse the request payload into a real path, then the DELETE endpoint will work properly
-        Storage::disk(config('canvas.storage_disk'))->delete(request()->getContent());
+        $file = pathinfo(request()->getContent());
+        $storagePath = $this->baseStoragePath();
+        $path = "{$storagePath}/{$file['basename']}";
 
-        return response()->json([], 204);
+        $fileDeleted = Storage::disk(config('canvas.storage_disk'))->delete($path);
+
+        if ($fileDeleted) {
+            return response()->json([], 204);
+        }
+    }
+
+    /**
+     * Return the storage path url.
+     *
+     * @return string
+     */
+    private function baseStoragePath(): string
+    {
+        return sprintf('%s/%s', config('canvas.storage_path'), 'images');
     }
 }
