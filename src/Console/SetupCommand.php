0124346334d47bf2d9662faa394aa95fdde303a7
Alberto Elias <albertoelias.amaral@yahoo.com>
2020-01-26 03:08:09 -0300

    Update qtd factories

--- src/Console/SetupCommand.php
+++ src/Console/SetupCommand.php
@@ -75,13 +75,13 @@ class SetupCommand extends Command
         $users = factory(\App\User::class, 5)->create();
         
         // Seed the posts data
-        $posts = factory(\Canvas\Post::class, 20)->create();
+        $posts = factory(\Canvas\Post::class, 50)->create();
 
         // Seed the tags data
-        $tags = factory(\Canvas\Tag::class, 5)->create();
+        $tags = factory(\Canvas\Tag::class, 15)->create();
 
         // Seed the topics data
-        $topics = factory(\Canvas\Topic::class, 5)->create();
+        $topics = factory(\Canvas\Topic::class, 10)->create();
 
         // Associate tags and topics with the posts
         $posts->each(function ($post) use ($tags, $topics) {
