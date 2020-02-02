e438e9549e5bb8e26bd32dda8caa5dd795974cea
Todd Austin <austin.todd.j@gmail.com>
2020-02-02 15:30:01 -0600

    icon updates

--- resources/js/components/editor/ImageModal.vue
+++ resources/js/components/editor/ImageModal.vue
@@ -65,9 +65,9 @@
                     <div v-if="!isSearchingUnsplash && !unsplashImages.length">
                         <div v-if="selectedImageUrl && !selectedImagesForPond.length && !isReadyToAcceptUploads" class="selected-image">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click.prevent="clearAndResetComponent">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" class="icon-close-circle">
-                                    <circle cx="12" cy="12" r="10" class="primary"/>
-                                    <path class="fill-bg" d="M13.41 12l2.83 2.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 1 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12z"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" class="icon-trash">
+                                    <path class="primary" d="M5 5h14l-.89 15.12a2 2 0 0 1-2 1.88H7.9a2 2 0 0 1-2-1.88L5 5zm5 5a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1zm4 0a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1z"/>
+                                    <path class="primary" d="M8.59 4l1.7-1.7A1 1 0 0 1 11 2h2a1 1 0 0 1 .7.3L15.42 4H19a1 1 0 0 1 0 2H5a1 1 0 1 1 0-2h3.59z"/>
                                 </svg>
                             </button>
                             <img :src="selectedImageUrl" class="w-100 rounded mb-3"/>
