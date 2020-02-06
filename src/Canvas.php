00aeaeba876c72eb5f50a75a1e43c908e82b6419
Todd Austin <austin.todd.j@gmail.com>
2020-02-06 06:42:53 -0600

    Apply fixes from StyleCI (#620)

--- src/Canvas.php
+++ src/Canvas.php
@@ -40,7 +40,7 @@ class Canvas
         $path = public_path('vendor/canvas/mix-manifest.json');
 
         if (! File::exists($path)) {
-            throw new RuntimeException(__('canvas::app.assets_are_not_up_to_date') . __('canvas::app.to_update_run') .' php artisan canvas:publish');
+            throw new RuntimeException(__('canvas::app.assets_are_not_up_to_date').__('canvas::app.to_update_run').' php artisan canvas:publish');
         }
 
         return File::get($path) === File::get(__DIR__.'/../public/mix-manifest.json');
