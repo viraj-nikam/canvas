cafa8728fba4fe0ab42260ef772f05dc50318854
robertgarrigos <robert@garrigos.cat>
2020-02-25 09:59:27 +0100

    Catalan translation

---
+++ resources/lang/ca/blog.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'title'   => 'Blog',
+    'buttons' => [
+        'learn' => 'Sapigueu més',
+        'continue' => 'Llegiu més...',
+        'next'  => 'Llegiu això següent',
+        'enjoy' => 'You might enjoy',
+        'edit'  => 'Edit post',
+        'stats' => 'View stats',
+        'canonical' => 'Publicat originalment a',
+    ],
+    'empty'   => [
+        'description' => 'No s\'ha trobat cap entrada, comenceu',
+        'action'      => 'tot afegint una nova entrada',
+    ],
+    'nav'     => [
+        'user' => [
+            'posts'  => 'Entrades',
+            'tags'   => 'Etiquetes',
+            'topics' => 'Temes',
+            'stats'  => 'Estadístiques',
+            'login'  => 'Entreu',
+            'logout' => 'Sortiu',
+        ],
+    ],
+    'posts'   => [
+        'label' => 'Entrades recents',
+        'reading' => 'Llegiu més...',
+    ],
+    'tags'    => [
+        'label' => 'Etiquetes',
+    ],
+
+];
