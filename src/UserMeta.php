59c33a47e8aaba25cee35dae1bbb044c6c373719
Micheal Mand <micheal@kmdwebdesigns.com>
2020-03-17 12:21:07 -0600

    Also use config on `UserMeta` model
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- src/UserMeta.php
+++ src/UserMeta.php
@@ -39,7 +39,7 @@ class UserMeta extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(config('canvas.user', User::class));
     }
 
     /**
