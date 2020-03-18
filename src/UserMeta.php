a2830ca9efe624daa7ee00e9285904d95a0b0416
Micheal Mand <micheal@kmdwebdesigns.com>
2020-03-17 20:59:32 -0600

    Remove fallback from config usage
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- src/UserMeta.php
+++ src/UserMeta.php
@@ -39,7 +39,7 @@ class UserMeta extends Model
      */
     public function user()
     {
-        return $this->belongsTo(config('canvas.user', User::class));
+        return $this->belongsTo(config('canvas.user'));
     }
 
     /**
