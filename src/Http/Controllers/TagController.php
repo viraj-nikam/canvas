8a713c4b073052fedbbf9b438e172cf9d2989dc5
Todd Austin <austin.todd.j@gmail.com>
2020-04-02 06:47:13 -0500

    fixes #692

--- src/Http/Controllers/TagController.php
+++ src/Http/Controllers/TagController.php
@@ -79,7 +79,7 @@ class TagController extends Controller
                 'alpha_dash',
                 Rule::unique('canvas_tags')->where(function ($query) use ($data) {
                     return $query->where('slug', $data['slug'])->where('user_id', $data['user_id']);
-                })->ignore($id)->whereNull('deleted_at'),
+                })->ignore($this->isNewTag($id) ? null : $id)->whereNull('deleted_at'),
             ],
         ], $messages)->validate();
 
