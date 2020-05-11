f359c0e4eee3f638d14e9f5758be5c08cdfc27ba
Todd Austin <austin.todd.j@gmail.com>
2020-05-10 20:56:41 -0500

    Apply fixes from StyleCI (#739)

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\File;
 
-if (!function_exists('is_fresh')) {
+if (! function_exists('is_fresh')) {
     /**
      * Return true if the given ID is for a new resource.
      *
@@ -15,7 +15,7 @@ if (!function_exists('is_fresh')) {
     }
 }
 
-if (!function_exists('assets_up_to_date')) {
+if (! function_exists('assets_up_to_date')) {
     /**
      * Return true if the public assets are up to date.
      *
@@ -25,10 +25,10 @@ if (!function_exists('assets_up_to_date')) {
     {
         $path = public_path('vendor/canvas/mix-manifest.json');
 
-        if (!File::exists($path)) {
-            throw new RuntimeException(__('canvas::app.assets_are_not_up_to_date') . __('canvas::app.to_update_run') . ' php artisan canvas:publish');
+        if (! File::exists($path)) {
+            throw new RuntimeException(__('canvas::app.assets_are_not_up_to_date').__('canvas::app.to_update_run').' php artisan canvas:publish');
         }
 
-        return File::get($path) === File::get(__DIR__ . '/../public/mix-manifest.json');
+        return File::get($path) === File::get(__DIR__.'/../public/mix-manifest.json');
     }
 }
