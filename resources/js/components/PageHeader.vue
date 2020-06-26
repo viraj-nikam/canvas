6f792ec5da77d9412b63be98c10386a9a82f6282
Todd Austin <austin.todd.j@gmail.com>
2020-06-26 14:48:26 -0500

    wip

--- resources/js/components/PageHeader.vue
+++ resources/js/components/PageHeader.vue
@@ -2,39 +2,34 @@
     <div class="shadow">
         <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1 col-md-12">
             <nav class="navbar d-flex px-0 py-0">
-                <router-link to="/" class="navbar-brand pt-0">
+                <router-link to="/" class="navbar-brand pt-0 hover">
                     canvas
                 </router-link>
 
-<!--                <div class="mx-2 d-flex flex-grow-1 justify-content-center">-->
-<!--                    <div class="input-group">-->
-<!--                        <div class="input-group-prepend mr-0 border-0">-->
-<!--                            <div class="input-group-text pr-0 border-0">-->
-<!--                                <svg-->
-<!--                                    xmlns="http://www.w3.org/2000/svg"-->
-<!--                                    viewBox="0 0 24 24"-->
-<!--                                    width="20"-->
-<!--                                    class="icon-search"-->
-<!--                                >-->
-<!--                                    <circle cx="10" cy="10" r="7" style="fill: none;" />-->
-<!--                                    <path-->
-<!--                                        class="fill-muted"-->
-<!--                                        d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"-->
-<!--                                    />-->
-<!--                                </svg>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <input class="form-control border-0" type="text" placeholder="Search..." />-->
-<!--                    </div>-->
-<!--                </div>-->
+                <!--                <div class="mx-2 d-flex flex-grow-1 justify-content-center">-->
+                <!--                    <div class="input-group">-->
+                <!--                        <div class="input-group-prepend mr-0 border-0">-->
+                <!--                            <div class="input-group-text pr-0 border-0">-->
+                <!--                                <svg-->
+                <!--                                    xmlns="http://www.w3.org/2000/svg"-->
+                <!--                                    viewBox="0 0 24 24"-->
+                <!--                                    width="20"-->
+                <!--                                    class="icon-search"-->
+                <!--                                >-->
+                <!--                                    <circle cx="10" cy="10" r="7" style="fill: none;" />-->
+                <!--                                    <path-->
+                <!--                                        class="fill-muted"-->
+                <!--                                        d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"-->
+                <!--                                    />-->
+                <!--                                </svg>-->
+                <!--                            </div>-->
+                <!--                        </div>-->
+                <!--                        <input class="form-control border-0" type="text" placeholder="Search..." />-->
+                <!--                    </div>-->
+                <!--                </div>-->
 
                 <a href="#" class="hover-light ml-auto">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="22"
-                        class="icon-search pr-1"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" class="icon-search pr-1">
                         <circle cx="10" cy="10" r="7" style="fill: none;" />
                         <path
                             class="fill-light-gray"
@@ -140,10 +135,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    @import '../../sass/utilities/variables';
+@import '../../sass/utilities/variables';
 
-    .navbar-brand {
-        font-size: 2.5rem;
-        font-family: 'Caveat', serif;
-    }
+.navbar-brand {
+    font-size: 2.5rem;
+    font-family: 'Caveat', serif;
+}
 </style>
