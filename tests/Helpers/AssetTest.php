d85291f2625ab223fed74605c5b5896f8fe43797
Todd Austin <austin.todd.j@gmail.com>
2020-07-04 14:19:42 -0500

    wip

---
+++ tests/Helpers/AssetTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Canvas\Tests\Helpers;
+
+use Canvas\Helpers\Asset;
+use Canvas\Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+use Mockery\Mock;
+use RuntimeException;
+
+class AssetTest extends TestCase
+{
+    /** @test */
+    public function it_can_check_for_up_to_date_published_assets()
+    {
+        $this->expectException(RuntimeException::class);
+
+        Asset::upToDate();
+    }
+}
