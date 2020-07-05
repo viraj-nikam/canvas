0932001390c3f53578ce6d4ef4598c142677e390
Todd Austin <austin.todd.j@gmail.com>
2020-07-05 09:37:35 -0500

    wip

--- resources/js/tests/components/PageHeader.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import PageHeader from '../../components/PageHeader';
-const { it, expect } = global;
-
-it('is a Vue instance', () => {
-    const wrapper = shallowMount(PageHeader);
-
-    expect(wrapper.isVueInstance()).toBeTruthy();
-});
