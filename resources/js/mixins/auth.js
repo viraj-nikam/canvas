e93126cae3efb2a006ddfca924f18e9561535d23
Todd Austin <austin.todd.j@gmail.com>
2020-08-13 15:07:35 -0500

    wip

---
+++ resources/js/mixins/auth.js
@@ -0,0 +1,9 @@
+import { store } from '../store';
+
+export default {
+    methods: {
+        userIsAdmin() {
+            return store.state.auth.admin === 1;
+        }
+    },
+};
--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
 import AllStats from '../views/AllStats';
 import PostStats from '../views/PostStats';
 import PostList from '../views/PostList';
@@ -12,12 +11,10 @@ import TopicList from '../views/TopicList';
 import EditSettings from '../views/EditSettings';
 import EditUser from '../views/EditUser';
 import UserList from '../views/UserList';
-
 import { store } from '../store';
+import auth from "../mixins/auth";
 
 Vue.use(Router);
-
-let auth = store.state.auth;
 
 export default [
     {
@@ -55,11 +52,7 @@ export default [
         name: 'tags',
         component: TagList,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -67,11 +60,7 @@ export default [
         name: 'create-tag',
         component: EditTag,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -79,11 +68,7 @@ export default [
         name: 'edit-tag',
         component: EditTag,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -91,11 +76,7 @@ export default [
         name: 'topics',
         component: TopicList,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -103,11 +84,7 @@ export default [
         name: 'create-topic',
         component: EditTopic,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -115,11 +92,7 @@ export default [
         name: 'edit-topic',
         component: EditTopic,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -132,11 +105,7 @@ export default [
         name: 'users',
         component: UserList,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1) {
-                next();
-            } else {
-                next({ name: 'home' });
-            }
+            auth.userIsAdmin() ? next() : next({ name: 'home' });
         },
     },
     {
@@ -144,7 +113,7 @@ export default [
         name: 'edit-user',
         component: EditUser,
         beforeEnter: (to, from, next) => {
-            if (auth.admin === 1 || auth.id === to.params.id) {
+            if (auth.userIsAdmin() || store.state.auth.id == to.params.id) {
                 next();
             } else {
                 next({ name: 'home' });
