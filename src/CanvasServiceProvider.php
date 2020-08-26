5f493acf5e706fe65c8b1bea6297c3edb1d80487
Todd Austin <austin.todd.j@gmail.com>
2020-08-26 06:50:53 -0500

    Apply fixes from StyleCI (#802)

--- src/CanvasServiceProvider.php
+++ src/CanvasServiceProvider.php
@@ -14,112 +14,112 @@ use Illuminate\Support\ServiceProvider;
 
 class CanvasServiceProvider extends ServiceProvider
 {
-/**
-* Bootstrap any package services.
-*
-* @return void
-* @throws BindingResolutionException
-*/
-public function boot()
-{
-$this->registerEvents();
-$this->registerRoutes();
-$this->registerMigrations();
-$this->registerPublishing();
-$this->loadViewsFrom(__DIR__.'/../resources/views', 'canvas');
-$this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'canvas');
-}
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     * @throws BindingResolutionException
+     */
+    public function boot()
+    {
+        $this->registerEvents();
+        $this->registerRoutes();
+        $this->registerMigrations();
+        $this->registerPublishing();
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'canvas');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'canvas');
+    }
 
-/**
-* Register bindings in the container.
-*
-* @return void
-*/
-public function register()
-{
-$this->mergeConfigFrom(
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
     __DIR__.'/../config/canvas.php',
     'canvas'
 );
 
-$this->commands([
-    DigestCommand::class,
-    InstallCommand::class,
-    PublishCommand::class,
-]);
-}
+        $this->commands([
+            DigestCommand::class,
+            InstallCommand::class,
+            PublishCommand::class,
+        ]);
+    }
 
-/**
-* Register the events and listeners.
-*
-* @return void
-* @throws BindingResolutionException
-*/
-private function registerEvents()
-{
-$events = $this->app->make(Dispatcher::class);
+    /**
+     * Register the events and listeners.
+     *
+     * @return void
+     * @throws BindingResolutionException
+     */
+    private function registerEvents()
+    {
+        $events = $this->app->make(Dispatcher::class);
 
-$mappings = [
-    PostViewed::class => [
-        CaptureView::class,
-        CaptureVisit::class,
-    ],
+        $mappings = [
+            PostViewed::class => [
+                CaptureView::class,
+                CaptureVisit::class,
+            ],
 ];
 
-foreach ($mappings as $event => $listeners) {
-    foreach ($listeners as $listener) {
-        $events->listen($event, $listener);
-    }
-}
-}
+                foreach ($mappings as $event => $listeners) {
+                    foreach ($listeners as $listener) {
+                        $events->listen($event, $listener);
+                    }
+                }
+            }
 
-/**
-* Register the package routes.
-*
-* @return void
-*/
-private function registerRoutes()
-{
-$this->loadRoutesFrom(__DIR__.'/Http/routes.php');
-}
+            /**
+             * Register the package routes.
+             *
+             * @return void
+             */
+            private function registerRoutes()
+            {
+                $this->loadRoutesFrom(__DIR__.'/Http/routes.php');
+            }
 
-/**
-* Register the package's migrations.
-*
-* @return void
-*/
-private function registerMigrations()
-{
-if ($this->app->runningInConsole()) {
-    $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-}
-}
+            /**
+             * Register the package's migrations.
+             *
+             * @return void
+             */
+            private function registerMigrations()
+            {
+                if ($this->app->runningInConsole()) {
+                    $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+                }
+            }
 
-/**
-* Register the package's publishable resources.
-*
-* @return void
-*/
-private function registerPublishing()
-{
-if ($this->app->runningInConsole()) {
-    $this->publishes([
-        __DIR__.'/../public' => public_path('vendor/canvas'),
-    ], 'canvas-assets');
+            /**
+             * Register the package's publishable resources.
+             *
+             * @return void
+             */
+            private function registerPublishing()
+            {
+                if ($this->app->runningInConsole()) {
+                    $this->publishes([
+                        __DIR__.'/../public' => public_path('vendor/canvas'),
+                    ], 'canvas-assets');
 
-    $this->publishes([
-        __DIR__.'/../config/canvas.php' => config_path('canvas.php'),
-    ], 'canvas-config');
+                    $this->publishes([
+                        __DIR__.'/../config/canvas.php' => config_path('canvas.php'),
+                    ], 'canvas-config');
 
-    $this->publishes([
-        __DIR__.'/../resources/lang' => resource_path('lang/vendor/canvas'),
-    ], 'canvas-lang');
+                    $this->publishes([
+                        __DIR__.'/../resources/lang' => resource_path('lang/vendor/canvas'),
+                    ], 'canvas-lang');
 
-    $this->publishes([
-        __DIR__.'/../resources/stubs/CanvasServiceProvider.stub' => app_path(
-            'Providers/CanvasServiceProvider.php'
-        ),
-    ], 'canvas-provider');
-}
-}
+                    $this->publishes([
+                        __DIR__.'/../resources/stubs/CanvasServiceProvider.stub' => app_path(
+                            'Providers/CanvasServiceProvider.php'
+                        ),
+                    ], 'canvas-provider');
+                }
+            }
 }
