6834b1960391108d1b3dab3800cec3257405eb1f
Todd Austin <austin.todd.j@gmail.com>
2020-09-20 15:02:42 -0500

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 /*
  |--------------------------------------------------------------------------
@@ -7,7 +6,7 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
@@ -24,11 +23,6 @@ mix.options({
 
 mix.setPublicPath('public')
     .setResourceRoot('../')
-    // .webpackConfig({
-    //     plugins: [
-    //         new BundleAnalyzerPlugin(),
-    //     ],
-    // })
     .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .version()
