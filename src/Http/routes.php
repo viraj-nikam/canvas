231d745e391441285ac863bb0276fb57741dd19a
Todd Austin <austin.todd.j@gmail.com>
2020-09-25 19:20:54 -0500

    route updates for Laravel 8

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,16 @@
 <?php
 
+use Canvas\Http\Controllers\Auth\ForgotPasswordController;
+use Canvas\Http\Controllers\Auth\LoginController;
+use Canvas\Http\Controllers\Auth\ResetPasswordController;
+use Canvas\Http\Controllers\HomeController;
+use Canvas\Http\Controllers\PostController;
+use Canvas\Http\Controllers\SearchController;
+use Canvas\Http\Controllers\StatsController;
+use Canvas\Http\Controllers\TagController;
+use Canvas\Http\Controllers\TopicController;
+use Canvas\Http\Controllers\UploadsController;
+use Canvas\Http\Controllers\UserController;
 use Canvas\Http\Middleware\Admin;
 use Canvas\Http\Middleware\Authorize;
 use Illuminate\Support\Facades\Route;
@@ -12,71 +23,71 @@ Route::namespace('Auth')->group(function () {
     });
 
     Route::prefix('password')->group(function () {
-        Route::get('reset', 'ForgotPasswordController@showLinkRequestForm')->name('canvas.password.request');
-        Route::post('email', 'ForgotPasswordController@sendResetLinkEmail')->name('canvas.password.email');
-        Route::get('reset/{token}', 'ResetPasswordController@showResetForm')->name('canvas.password.reset');
-        Route::post('reset', 'ResetPasswordController@reset')->name('canvas.password.update');
+        Route::get('reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('canvas.password.request');
+        Route::post('email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('canvas.password.email');
+        Route::get('reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('canvas.password.reset');
+        Route::post('reset', [ResetPasswordController::class, 'reset'])->name('canvas.password.update');
     });
 
-    Route::get('logout', 'LoginController@logout')->name('canvas.logout');
+    Route::get('logout', [LoginController::class, 'logout'])->name('canvas.logout');
 });
 
 // API routes...
 Route::middleware([Authorize::class])->group(function () {
     Route::prefix('api')->group(function () {
         Route::prefix('uploads')->group(function () {
-            Route::post('/', 'UploadsController@store');
-            Route::delete('/', 'UploadsController@destroy');
+            Route::post('/', [UploadsController::class, 'store']);
+            Route::delete('/', [UploadsController::class, 'destroy']);
         });
 
         Route::prefix('posts')->group(function () {
-            Route::get('/', 'PostController@index');
-            Route::get('create', 'PostController@create');
-            Route::get('{id}', 'PostController@show');
-            Route::post('{id}', 'PostController@store');
-            Route::delete('{id}', 'PostController@destroy');
+            Route::get('/', [PostController::class, 'index']);
+            Route::get('create', [PostController::class, 'create']);
+            Route::get('{id}', [PostController::class, 'show']);
+            Route::post('{id}', [PostController::class, 'store']);
+            Route::delete('{id}', [PostController::class, 'destroy']);
         });
 
         Route::prefix('stats')->group(function () {
-            Route::get('/', 'StatsController@index');
-            Route::get('{id}', 'StatsController@show');
+            Route::get('/', [StatsController::class, 'index']);
+            Route::get('{id}', [StatsController::class, 'show']);
         });
 
         Route::prefix('tags')->middleware([Admin::class])->group(function () {
-            Route::get('/', 'TagController@index');
-            Route::get('create', 'TagController@create');
-            Route::get('{id}', 'TagController@show');
-            Route::get('{id}/posts', 'TagController@showPosts');
-            Route::post('{id}', 'TagController@store');
-            Route::delete('{id}', 'TagController@destroy');
+            Route::get('/', [TagController::class, 'index']);
+            Route::get('create', [TagController::class, 'create']);
+            Route::get('{id}', [TagController::class, 'show']);
+            Route::get('{id}/posts', [TagController::class, 'showPosts']);
+            Route::post('{id}', [TagController::class, 'store']);
+            Route::delete('{id}', [TagController::class, 'destroy']);
         });
 
         Route::prefix('topics')->middleware([Admin::class])->group(function () {
-            Route::get('/', 'TopicController@index');
-            Route::get('create', 'TopicController@create');
-            Route::get('{id}', 'TopicController@show');
-            Route::get('{id}/posts', 'TopicController@showPosts');
-            Route::post('{id}', 'TopicController@store');
-            Route::delete('{id}', 'TopicController@destroy');
+            Route::get('/', [TopicController::class, 'index']);
+            Route::get('create', [TopicController::class, 'create']);
+            Route::get('{id}', [TopicController::class, 'show']);
+            Route::get('{id}/posts', [TopicController::class, 'showPosts']);
+            Route::post('{id}', [TopicController::class, 'store']);
+            Route::delete('{id}', [TopicController::class, 'destroy']);
         });
 
         Route::prefix('users')->group(function () {
-            Route::get('/', 'UserController@index')->middleware([Admin::class]);
-            Route::get('create', 'UserController@create')->middleware([Admin::class]);
-            Route::get('{id}', 'UserController@show');
-            Route::get('{id}/posts', 'UserController@showPosts');
-            Route::post('{id}', 'UserController@store');
-            Route::delete('{id}', 'UserController@destroy')->middleware([Admin::class]);
+            Route::get('/', [UserController::class, 'index'])->middleware([Admin::class]);
+            Route::get('create', [UserController::class, 'create'])->middleware([Admin::class]);
+            Route::get('{id}', [UserController::class, 'show']);
+            Route::get('{id}/posts', [UserController::class, 'showPosts']);
+            Route::post('{id}', [UserController::class, 'store']);
+            Route::delete('{id}', [UserController::class, 'destroy'])->middleware([Admin::class]);
         });
 
         Route::prefix('search')->group(function () {
-            Route::get('posts', 'SearchController@showPosts');
-            Route::get('tags', 'SearchController@showTags')->middleware([Admin::class]);
-            Route::get('topics', 'SearchController@showTopics')->middleware([Admin::class]);
-            Route::get('users', 'SearchController@showUsers')->middleware([Admin::class]);
+            Route::get('posts', [SearchController::class, 'showPosts']);
+            Route::get('tags', [SearchController::class, 'showTags'])->middleware([Admin::class]);
+            Route::get('topics', [SearchController::class, 'showTopics'])->middleware([Admin::class]);
+            Route::get('users', [SearchController::class, 'showUsers'])->middleware([Admin::class]);
         });
     });
 
     // Catch-all route...
-    Route::get('/{view?}', 'HomeController@index')->where('view', '(.*)')->name('canvas');
+    Route::get('/{view?}', [HomeController::class, 'index'])->where('view', '(.*)')->name('canvas');
 });
