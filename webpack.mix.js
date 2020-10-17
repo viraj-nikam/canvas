30767fbe620ef854323719c246bca725ed83a5fe
Todd Austin <austin.todd.j@gmail.com>
2020-10-17 08:43:29 -0500

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +11,12 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix.webpackConfig({
+    plugins: [
+        // new BundleAnalyzerPlugin(),
+    ],
+});
 
 mix.options({
     uglify: {
