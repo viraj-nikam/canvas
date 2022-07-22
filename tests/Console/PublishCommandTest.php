2a5b742d58c2422c59ed9468f6933770c8b8874e
Todd Austin <austin.todd.j@gmail.com>
2022-07-22 11:51:39 -0500

    fix for the failing tests and dependency upgrades

--- tests/Console/PublishCommandTest.php
+++ tests/Console/PublishCommandTest.php
@@ -13,11 +13,8 @@ class PublishCommandTest extends TestCase
 {
     public function testCanvasPublishCommand(): void
     {
-        // No idea why this prints out 3 times here... ¯\_(ツ)_/¯
         $this->artisan('canvas:publish')
              ->assertExitCode(0)
-             ->expectsOutput('Publishing complete.')
-             ->expectsOutput('Publishing complete.')
              ->expectsOutput('Publishing complete.');
     }
 }
